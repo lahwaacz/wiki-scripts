6bb3ecd4fd46eb4098b012873970aa0db326f868
Jakub Klinkovský <j.l.k@gmx.com>
2014-07-10 00:59:17 +0200

    add list-article-summaries.py script

---
+++ list-article-summaries.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+import os.path
+
+from MediaWiki import API
+from utils import *
+
+api_url = "https://wiki.archlinux.org/api.php"
+cookie_path = os.path.expanduser("~/.cache/ArchWiki.cookie")
+
+api = API(api_url, cookie_file=cookie_path, ssl_verify=True)
+
+# print only easy-to-fix pages
+as_in = api.generator(generator="embeddedin", geilimit="max", geititle="Template:Article summary wiki")
+#as_out = flatten_gen( (api.generator(generator="embeddedin", geilimit="max", geititle=title) for title in ["Template:Article summary heading", "Template:Article summary link", "Template:Article summary text"]) )
+as_out = flatten_gen( (api.generator(generator="embeddedin", geilimit="max", geititle=title) for title in ["Template:Article summary link"]) )
+
+titles_out = [p["title"] for p in as_out if p["ns"] == 0]
+
+for page in sorted(as_in , key=lambda d: d["title"]):
+    title = page["title"]
+    if page["ns"] == 0 and title not in titles_out:
+        print("* [[%s]]" % title)
+#        print("** https://wiki.archlinux.org/index.php/%s" % title.replace(" ", "_"))
