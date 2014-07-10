44f3558721560a422c6f201a5cc2769a68367825
Jakub Klinkovský <j.l.k@gmx.com>
2014-07-10 12:06:56 +0200

    add API.list() method

--- MediaWiki/api.py
+++ MediaWiki/api.py
@@ -111,6 +111,27 @@ class API(Connection):
             # API generator returns dict !!!
             # for example:  snippet === {"pages":
             #       {"9693": {"title": "Page title", "ns": 0, "pageid": "9693"},
-            #       {"1165", {"title": ...
+            #        "1165", {"title": ...
             for _, page in snippet["pages"].items():
                 yield page
+
+    def list(self, params=None, **kwargs):
+        """
+        Interface to API:Lists, implemented as Python generator.
+
+        Parameter ``list`` must be supplied.
+
+        :param params: same as :py:func:`API.query_continue`
+        :param kwargs: same as :py:func:`API.query_continue`
+        :yields: from ``list`` part of the API response
+        """
+        list_ = kwargs.get("list") if params is None else params.get("list")
+        if list_ is None:
+            raise ValueError("param 'list' must be supplied")
+
+        for snippet in self.query_continue(params, **kwargs):
+            # API list returns list !!!
+            # example for list="allpages":  snippet === {"allpages":
+            #       [{"title": "Page title", "ns": 0, "pageid": "9693"},
+            #        {"title": ...
+            yield from snippet[list_]
