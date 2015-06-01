d61327ed4841c0063af94c19a09d5daa54211293
Jakub Klinkovský <j.l.k@gmx.com>
2015-06-01 20:52:21 +0200

    implement the 'edit' action for interactive prompt
    raise InteractiveQuit instead of calling sys.exit(1) (fixes #7)

--- MediaWiki/interactive.py
+++ MediaWiki/interactive.py
@@ -5,9 +5,14 @@ Collection of functions extending :py:class:`MediaWiki.api.API` with various
 interactive tasks.
 """
 
+import os
 import sys
 import getpass
+import subprocess
+
 from .diff import diff_highlighted
+
+__all__ = ["require_login", "edit_interactive", "InteractiveQuit"]
 
 def require_login(api):
     """
@@ -24,30 +29,72 @@ def require_login(api):
             print("Login failed.", file=sys.stderr)
             sys.exit(1)
 
-def edit_interactive(api, pageid, old_text, new_text, basetimestamp, summary, **kwargs):
+class TmpFileSeries:
+    """
+    Resource management wrapper around a series of temporary files. Use it with the
+    `with` statement.
+
+    Reference: http://stackoverflow.com/questions/865115/how-do-i-correctly-clean-up-a-python-object/865272#865272
+    """
+    def __init__(self, basename, text_new, text_old, suffix="mediawiki", dir="/tmp"):
+        self.fname_new = "{}/{}.new.{}".format(dir, basename, suffix)
+        self.file_new = open(self.fname_new, "w+")
+        self.file_new.write(text_new)
+        self.file_new.flush()
+
+        self.fname_old = "{}/{}.old.{}".format(dir, basename, suffix)
+        self.file_old = open(self.fname_old, "w+")
+        self.file_old.write(text_old)
+        self.file_old.flush()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        os.unlink(self.fname_new)
+        os.unlink(self.fname_old)
+
+class InteractiveQuit(Exception):
+    """
+    Raised when the user specified `quit` on the interactive prompt.
+    """
+    pass
+
+# TODO: needs 'title' argument (to be shown in diff and for aptly named tmpfiles)
+# TODO: vimdiff should be configurable (depends on #3)
+def edit_interactive(api, pageid, text_old, text_new, basetimestamp, summary, **kwargs):
     # TODO: docstring
-    diff = diff_highlighted(old_text, new_text)
     options = [
         ("y", "make this edit"),
         ("n", "do not make this edit"),
         ("q", "quit; do not make this edit or any of the following"),
-# TODO:
-#            ("e", "manually edit this edit"),
+        ("e", "manually edit this edit"),
         ("?", "print help"),
     ]
     short_options = [opt[0] for opt in options]
     ans = ""
 
     while True:
+        diff = diff_highlighted(text_old, text_new)
         print(diff)
         ans = input("Make this edit? [%s]? " % ",".join(short_options))
+
         if ans == "?" or ans not in short_options:
             for opt in options:
                 print("%s - %s" % opt)
-        else:
+        elif ans == "y":
+            return api.edit(pageid, text_new, basetimestamp, summary, **kwargs)
+        elif ans == "n":
             break
-
-    if ans == "y":
-        return api.edit(pageid, new_text, basetimestamp, summary, **kwargs)
-    elif ans == "q":
-        sys.exit(1)
+        elif ans == "q":
+            raise InteractiveQuit
+        elif ans == "e":
+            with TmpFileSeries(pageid, text_new, text_old) as wrapper:
+                cmd = "vimdiff {} {}".format(wrapper.fname_new, wrapper.fname_old)
+                try:
+                    subprocess.check_call(cmd, shell=True)
+                    wrapper.file_new.seek(0)
+                    text_new = wrapper.file_new.read()
+                    print("Command '{}' exited succesfully.".format(cmd))
+                except subprocess.CalledProcessError as e:
+                    print("Failed command: '{}' (return code {})".format(cmd, e.returncode), file=sys.stderr)
