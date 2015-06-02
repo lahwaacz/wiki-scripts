b86d435b49feb13f8181f0fd9dda2dc51898b3b6
Jakub Klinkovský <j.l.k@gmx.com>
2015-06-02 20:02:06 +0200

    add docstring for canonicalize()

--- link-checker.py
+++ link-checker.py
@@ -23,9 +23,16 @@ from MediaWiki.interactive import *
 import ArchWiki.lang as lang
 
 def canonicalize(title):
-    title = title.strip()
+    """
+    Return a canonical form of the title, that is with underscores replaced with
+    spaces, leading and trailing whitespace stripped and first letter
+    capitalized.
+
+    :param title: a `str` or `mwparserfromhell.nodes.wikicode.Wikicode` object
+    :returns: a `str` object
+    """
+    title = str(title).replace("_", " ").strip()
     title = title[0].upper() + title[1:]
-    title = title.replace("_", " ")
     return title
 
 class LinkChecker:
