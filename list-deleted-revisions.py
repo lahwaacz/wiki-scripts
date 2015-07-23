e260aa80b0213c6fef0d4f95305e3097d39a8fb6
Jakub Klinkovský <j.l.k@gmx.com>
2015-07-23 14:51:43 +0200

    improve list-deleted-revisions.py script

--- list-deleted-revisions.py
+++ list-deleted-revisions.py
@@ -3,6 +3,8 @@
 import sys
 import os.path
 
+from pprint import pprint
+
 from MediaWiki import API
 from MediaWiki.interactive import require_login
 
@@ -17,5 +19,32 @@ if "deletedhistory" not in api.user_rights():
     print("The current user does not have the 'deletedhistory' right, which is necessary to use this script. Sorry.")
     sys.exit(1)
 
-for page in api.list(list="alldeletedrevisions", adrlimit="max"):
-    print(page["title"])
+pages = api.list(list="alldeletedrevisions", adrlimit="max")
+
+pages_counts = {}
+users_counts = {}
+
+for page in pages:
+    title = page["title"]
+    pages_counts.setdefault(title, 0)
+    for r in page.get("revisions", []):
+        # print revision
+        pprint(r)
+        # increment counters
+        pages_counts[title] += 1
+        user = r["user"]
+        users_counts.setdefault(user, 0)
+        users_counts[user] += 1
+
+# total count of deleted revisions
+total_count = sum(count for _, count in pages_counts.items())
+# count of pages with non-zero number of deleted revisions
+pages_count = len([1 for _, count in pages_counts.items() if count > 0])
+# count of users whose at least one revision has been deleted
+users_count = len(users_counts.keys())
+
+print("{} deleted revisions on {} pages by {} users".format(total_count, pages_count, users_count))
+
+# print top 20 users with most deleted revisions
+for user, count in sorted(users_counts.items(), key=lambda t: t[1], reverse=True)[:20]:
+    print(user, count)
