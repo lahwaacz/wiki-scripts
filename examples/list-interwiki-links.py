d55766ed1c704353d5c306dadf2d527e2ba8481a
Jakub Klinkovský <j.l.k@gmx.com>
2015-07-26 13:59:53 +0200

    add examples/list-interwiki-links.py

---
+++ examples/list-interwiki-links.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python3
+
+import os.path
+
+from ws.core import API
+
+api_urls = [
+    "https://wiki.archlinux.org/api.php",
+    "https://wiki.archlinux.de/api.php",
+]
+cookie_path = os.path.expanduser("~/.cache/ArchWiki.cookie")
+
+for api_url in api_urls:
+    api = API(api_url, cookie_file=cookie_path, ssl_verify=True)
+    prefixes = set()
+    for ns in api.namespaces():
+        if ns < 0:
+            continue
+        for page in api.generator(generator="allpages", gaplimit="max", gapfilterredir="nonredirects", gapnamespace=ns, prop="iwlinks", iwprop="url", iwlimit="max"):
+            for link in page.get("iwlinks", []):
+                prefixes.add(link["prefix"])
+    print("interwiki prefixes used on {}:".format(api.get_hostname()))
+    print(sorted(prefixes))
