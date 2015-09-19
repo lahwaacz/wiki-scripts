3a3d54c5481da59c6a844de20d51f3cf816241d0
Jakub Klinkovský <j.l.k@gmx.com>
2015-09-19 11:20:37 +0200

    rewrite valid_anchor() -> valid_sectionname()

--- list-redirects-broken-fragments.py
+++ list-redirects-broken-fragments.py
@@ -4,9 +4,6 @@
 #   pulling revisions from cache does not expand templates (transclusions like on List of applications)
 #   finally merge into link-checker.py, broken stuff should be just reported
 
-import re
-import collections
-
 from ws.core import API
 import ws.cache
 import ws.utils
@@ -14,34 +11,39 @@ from ws.parser_helpers.encodings import dotencode
 from ws.parser_helpers.title import Title
 from ws.parser_helpers.wikicode import get_anchors
 
-# TODO: split to get_section_headings() and get_anchors() and move to
-# ws.parser_helpers (without caching, title -> text)
-def valid_anchor(title, anchor, pages, wrapped_titles):
+def valid_sectionname(title, pages, wrapped_titles):
     """
-    Checks if given anchor is valid, i.e. if a corresponding section exists on
-    a page with given title.
+    Checks if the ``sectionname`` property of given title is valid, i.e. if a
+    corresponding section exists on a page with given title.
 
     .. note::
-        validation is limited to pages in the Main namespace for easier access
+        Validation is limited to pages in the Main namespace for easier access
         to the cache; anchors on other pages are considered to be always valid.
 
-    :param title: title of the target page
-    :param anchor: the section link anchor (without the ``#`` delimiter); may or
-                   may not be dot-encoded
+    :param title: parsed title of the wikilink to be checked
+    :type title: ws.parser_helpers.title.Title
     :returns: ``True`` if the anchor corresponds to an existing section
     """
-    _title = Title(api, title)
-    if _title.namespace != "":
-        # not really, but causes to take no action
+    # we can't check interwiki links
+    if title.iwprefix:
         return True
-    page = ws.utils.bisect_find(pages, title, index_list=wrapped_titles)
+
+    # TODO: limitation of the cache, we can easily check only the main namespace
+    if title.namespace != "":
+        return True
+
+    # empty sectionname is always valid
+    if title.sectionname == "":
+        return True
+
+    page = ws.utils.bisect_find(pages, title.fullpagename, index_list=wrapped_titles)
     text = page["revisions"][0]["*"]
 
     # get list of valid anchors
     anchors = get_anchors(text)
 
     # encode the given anchor and validate
-    return dotencode(anchor) in anchors
+    return dotencode(title.sectionname) in anchors
 
 def main(api, db):
     # limit to redirects pointing to the content namespaces
@@ -53,17 +55,13 @@ def main(api, db):
 
     for source in sorted(redirects.keys()):
         target = redirects[source]
-
-        # first limit to redirects with fragments
-        if len(target.split("#", maxsplit=1)) == 1:
-            continue
+        title = Title(api, target)
 
         # limit to redirects with broken fragment
-        target, fragment = target.split("#", maxsplit=1)
-        if valid_anchor(target, fragment, pages, wrapped_titles):
+        if valid_sectionname(title, pages, wrapped_titles):
             continue
 
-        print("* [[{}]] --> [[{}#{}]]".format(source, target, fragment))
+        print("* [[{}]] --> [[{}]]".format(source, target))
 
 if __name__ == "__main__":
     import ws.config
