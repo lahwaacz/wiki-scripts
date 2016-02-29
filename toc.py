1e5d0d7f6ed6cd74831f3c1d256b6cd6744bd969
Jakub Klinkovský <j.l.k@gmx.com>
2016-02-29 23:43:06 +0100

    refactoring toc.py

--- toc.py
+++ toc.py
@@ -6,58 +6,81 @@ from ws.core import API
 import ws.ArchWiki.lang as lang
 
 
-def build_graph(api):
-    # `graph_parents` maps category names to the list of their parents
-    graph_parents = {}
-    # `graph_subcats` maps category names to the list of their subcategories
-    graph_subcats = {}
-    # a mapping of category names to the corresponding "categoryinfo" dictionary
-    info = {}
-    for page in api.generator(generator="allpages", gaplimit="max", gapnamespace=14, prop="categories|categoryinfo", cllimit="max", clshow="!hidden", clprop="hidden"):
-        if "categories" in page:
-            graph_parents.setdefault(page["title"], []).extend([cat["title"] for cat in page["categories"]])
-            for cat in page["categories"]:
-                graph_subcats.setdefault(cat["title"], []).append(page["title"])
-        if "categoryinfo" in page:
-            info.setdefault(page["title"], {}).update(page["categoryinfo"])
-    return graph_parents, graph_subcats, info
+class TableOfContents:
 
-def main(api):
-    graph_parents, graph_subcats, info = build_graph(api)
-    roots = ["Category:English"]
+    def __init__(self, api):
+        self.api = api
 
-    levels = []
+    @staticmethod
+    def set_argparser(argparser):
+        # first try to set options for objects we depend on
+        present_groups = [group.title for group in argparser._action_groups]
+        if "Connection parameters" not in present_groups:
+            API.set_argparser(argparser)
 
-    def print_info(title, parent=None, levels=None):
-        if levels is None:
-            # root
-            print("{} ({})".format(title, info[title]["pages"]))
-        else:
-            # indent
-            output = " " * (len(levels) - 1) * 4
-            # level
-            output += ".".join(str(x + 1) for x in levels)
-            # title, number of subpages
-            output += " {} ({})".format(title, info[title]["pages"])
-            # "also in" suffix
-            parents = set(graph_parents[title]) - {parent}
-            if parents:
-                output += " (also in: {})".format(", ".join(sorted(parents)))
-            print(output)
+    @classmethod
+    def from_argparser(klass, args, api=None):
+        if api is None:
+            api = API.from_argparser(args)
+        return klass(api)
 
-    def walk(root):
-        nodes = graph_subcats.get(root, [])
-        for i, title in enumerate(sorted(nodes)):
-            levels.append(i)
-            print_info(title, root, levels)
-            walk(title)
-            levels.pop(-1)
+    def build_graph(self):
+        # `graph_parents` maps category names to the list of their parents
+        graph_parents = {}
+        # `graph_subcats` maps category names to the list of their subcategories
+        graph_subcats = {}
+        # a mapping of category names to the corresponding "categoryinfo" dictionary
+        info = {}
+        for page in self.api.generator(generator="allpages", gaplimit="max", gapnamespace=14, prop="categories|categoryinfo", cllimit="max", clshow="!hidden", clprop="hidden"):
+            if "categories" in page:
+                graph_parents.setdefault(page["title"], []).extend([cat["title"] for cat in page["categories"]])
+                for cat in page["categories"]:
+                    graph_subcats.setdefault(cat["title"], []).append(page["title"])
+            if "categoryinfo" in page:
+                info.setdefault(page["title"], {}).update(page["categoryinfo"])
+        return graph_parents, graph_subcats, info
 
-    for title in roots:
-        print_info(title)
-        walk(title)
+    def run(self):
+        graph_parents, graph_subcats, info = self.build_graph()
+        roots = ["Category:English"]
+
+        levels = []
+
+        def format_plain(title, parent=None, levels=None):
+            if levels is None:
+                # root
+                print("{} ({})".format(title, info[title]["pages"]))
+            else:
+                # indent
+                output = " " * (len(levels) - 1) * 4
+                # level
+                output += ".".join(str(x + 1) for x in levels)
+                # title, number of subpages
+                output += " {} ({})".format(title, info[title]["pages"])
+                # "also in" suffix
+                parents = set(graph_parents[title]) - {parent}
+                if parents:
+                    output += " (also in: {})".format(", ".join(sorted(parents)))
+                print(output)
+
+        def format_html(title, parent=None, levels=None):
+            pass
+
+        def walk(root):
+            nodes = graph_subcats.get(root, [])
+            for i, title in enumerate(sorted(nodes)):
+                levels.append(i)
+                yield title, root, levels
+                yield from walk(title)
+                levels.pop(-1)
+
+        ff = format_plain
+        for title in roots:
+            ff(title)
+            for item in walk(title):
+                ff(*item)
 
 if __name__ == "__main__":
     import ws.config
-    api = ws.config.object_from_argparser(API)
-    main(api)
+    toc = ws.config.object_from_argparser(TableOfContents, description="Build a presentation of the wiki's table of contents")
+    toc.run()
