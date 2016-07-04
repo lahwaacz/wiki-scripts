eb2fb096aef91c1fec6853dd291afb1e0fd13ee4
Jakub Klinkovský <j.l.k@gmx.com>
2016-07-04 22:05:46 +0200

    refactoring: generalize ws.core.user

---
+++ ws/core/meta.py
@@ -0,0 +1,77 @@
+#! /usr/bin/env python3
+
+import datetime
+
+class Meta:
+    """
+    Meta-class for ``siteinfo`` and ``userinfo`` functionality.
+
+    Subclasses must configure the :py:attr:`module` and :py:attr:`properties`
+    attributes.
+    """
+
+    module = ""
+    properties = set()
+    volatile_properties = set()
+    timeout = 0
+    volatile_timeout = 0
+
+    def __init__(self, api):
+        self._api = api
+        self._values = {}
+        self._timestamps = {}
+
+    # TODO: expand, move somewhere more suitable
+    @classmethod
+    def _abbreviation(klass):
+        abbreviations = {
+            "siteinfo": "si",
+            "userinfo": "ui",
+        }
+        if klass.module not in abbreviations:
+            raise NotImplementedError("The abbreviation of '{}' module is not known.".format(klass.module))
+        return abbreviations[klass.module]
+
+    def fetch(self, prop=None):
+        """
+        Auxiliary method for querying properties.
+        """
+        utcnow = datetime.datetime.utcnow()
+
+        data = {
+            "action": "query",
+            "meta": self.module,
+            }
+        if isinstance(prop, list):
+            data[self._abbreviation() + "prop"] = "|".join(prop)
+        # TODO: name and id are for userinfo, other modules might have their own 'special' properties
+        elif prop in self.properties - {"name", "id"}:
+            data[self._abbreviation() + "prop"] = prop
+        result = self._api.call_api(data)
+
+        # FIXME: WTF is siteinfo different than userinfo?
+        if self.module in result:
+            result = result[self.module]
+
+        self._values.update(result)
+        for prop in result:
+            self._timestamps[prop] = utcnow
+
+        if isinstance(prop, str):
+            return result[prop]
+        return result
+
+    def __getattr__(self, attr):
+        if attr not in self.properties:
+            raise AttributeError("Invalid attribute: '{}'. Valid attributes are: {}".format(attr, self.properties))
+
+        utcnow = datetime.datetime.utcnow()
+        if attr in self.volatile_properties:
+            delta = datetime.timedelta(seconds=self.volatile_timeout)
+        else:
+            delta = datetime.timedelta(seconds=self.timeout)
+
+        # don't fetch if delta is 0
+        if attr not in self._values or (delta and self._timestamps.get(attr, utcnow) < utcnow - delta):
+            self.fetch(attr)
+        return self._values[attr]
--- ws/core/user.py
+++ ws/core/user.py
@@ -2,9 +2,10 @@
 
 import datetime
 
+from .meta import Meta
 from .lazy import LazyProperty
 
-class User:
+class User(Meta):
     """
     The :py:class:`User` class holds and interacts with information about the
     current user.
@@ -12,55 +13,24 @@ class User:
     Valid properties are listed in the :py:attr:`properties` attribute.
     See the `MediaWiki documentation`_ for explanation of the properties.
 
-    All :py:attr:`properties` are evaluated lazily and cached with the
-    :py:class:`@LazyProperty <ws.core.lazy.LazyProperty>` decorator.
-    All cached properties are automatically invalidated after
-    :py:attr:`timeout` seconds, except :py:attr:`volatile_properties`, which
-    are invalidated after :py:attr:`volatile_timeout` seconds.
+    All :py:attr:`properties` are evaluated lazily and cached. All cached
+    properties are automatically invalidated after :py:attr:`timeout` seconds,
+    except :py:attr:`volatile_properties`, which are invalidated after
+    :py:attr:`volatile_timeout` seconds.
 
     .. _`MediaWiki documentation`: https://www.mediawiki.org/wiki/API:Userinfo
     """
 
-    properties = {"blockinfo", "hasmsg", "groups", "implicitgroups", "rights",
-            "changeablegroups", "options", "editcount", "ratelimits", "email",
-            "realname", "acceptlang", "registrationdate", "unreadcount"}
+    module = "userinfo"
+    properties = {"name", "id", "blockinfo", "hasmsg", "groups", "implicitgroups",
+            "rights", "changeablegroups", "options", "editcount", "ratelimits",
+            "email", "realname", "acceptlang", "registrationdate", "unreadcount"}
     volatile_properties = {"hasmsg", "editcount", "unreadcount"}
     timeout = 3600
     volatile_timeout = 300
 
     def __init__(self, api):
-        self._api = api
-        self._values = {}
-        self._timestamps = {}
-
-    def _get(self, uiprop=None):
-        """
-        Auxiliary method for querying a single property.
-        """
-        if uiprop is None:
-            result = self._api.call_api(action="query", meta="userinfo")
-            return result["userinfo"]
-        elif isinstance(uiprop, list):
-            result = self._api.call_api(action="query", meta="userinfo", uiprop="|".join(uiprop))
-            return result["userinfo"]
-        else:
-            result = self._api.call_api(action="query", meta="userinfo", uiprop=uiprop)
-            return result["userinfo"][uiprop]
-
-    def __getattr__(self, attr):
-        if attr not in self.properties:
-            raise AttributeError("Invalid attribute: '{}'. Valid attributes are: {}".format(attr, self.properties))
-
-        utcnow = datetime.datetime.utcnow()
-        if attr in self.volatile_properties:
-            threshold = utcnow - datetime.timedelta(seconds=self.volatile_timeout)
-        else:
-            threshold = utcnow - datetime.timedelta(seconds=self.timeout)
-
-        if attr not in self._values or self._timestamps[attr] < threshold:
-            self._values[attr] = self._get(attr)
-            self._timestamps[attr] = utcnow
-        return self._values[attr]
+        super().__init__(api)
 
     @LazyProperty
     def is_loggedin(self):
@@ -71,7 +41,7 @@ class User:
         The property is evaluated lazily and cached with the
         :py:class:`@LazyProperty <ws.core.lazy.LazyProperty>` decorator.
         """
-        return "anon" not in self._get()
+        return "anon" not in self._fetch()
 
     def set_option(self, option, value):
         """
