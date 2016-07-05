ad0eb35810b430b4c7b09dae1f80859f5e964651
Jakub Klinkovský <j.l.k@gmx.com>
2016-07-05 17:43:16 +0200

    expand API tests

--- tests/with_api/test_api.py
+++ tests/with_api/test_api.py
@@ -26,6 +26,10 @@ class test_api:
     def test_max_ids_per_query(self):
         assert_equals(fixtures.api.max_ids_per_query, 50)
 
+    @raises(ValueError)
+    def test_query_continue_dummy(self):
+        next(fixtures.api.query_continue(params=0))
+
     # testing on uncategorized categories (should contain only 5 items all the time)
     def test_query_continue(self):
         q = fixtures.api.query_continue(action="query", list="querypage", qppage="Uncategorizedcategories", qplimit=1)
@@ -34,12 +38,37 @@ class test_api:
             titles += [i["title"] for i in chunk["querypage"]["results"]]
         assert_equals(titles, self.uncat_cats)
 
+    def test_query_continue_params(self):
+        data = {
+            "list": "querypage",
+            "qppage": "Uncategorizedcategories",
+            "qplimit": 1,
+            }
+        q = fixtures.api.query_continue(data)
+        titles = []
+        for chunk in q:
+            titles += [i["title"] for i in chunk["querypage"]["results"]]
+        assert_equals(titles, self.uncat_cats)
+
+    def test_list_pagepropnames(self):
+        expected = ["displaytitle", "hiddencat", "newsectionlink", "noeditsection", "noindex", "notoc"]
+        pagepropnames = [d["propname"] for d in fixtures.api.list(list="pagepropnames")]
+        assert_equals(pagepropnames, expected)
+
+    @raises(ValueError)
+    def test_list_dummy(self):
+        next(fixtures.api.list())
+
     def test_list(self):
         q = fixtures.api.list(list="querypage", qppage="Uncategorizedcategories", qplimit="max")
         titles = []
         for i in q:
             titles.append(i["title"])
         assert_equals(titles, self.uncat_cats)
+
+    @raises(ValueError)
+    def test_generator_dummy(self):
+        next(fixtures.api.generator())
 
     def test_generator(self):
         q = fixtures.api.generator(generator="querypage", gqppage="Uncategorizedcategories", gqplimit="max")
@@ -49,7 +78,7 @@ class test_api:
         assert_equals(titles, self.uncat_cats)
 
     def test_resolve_redirects(self):
-        pageids = [1216, 17719]
+        pageids = [1216, 17719, *range(50)]
         expected = [
             {'from': 'ABS', 'to': 'Arch Build System'},
             {'from': 'Main Page', 'to': 'Main page'},
