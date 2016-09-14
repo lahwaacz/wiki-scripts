62aab804bb0572ba24f8da456929567915a8d25e
Jakub Klinkovský <j.l.k@gmx.com>
2016-09-14 13:21:15 +0200

    add files for tox

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+import ws
+
+setup(
+    name = "wiki-scripts",
+    version = ws.__version__,
+    packages = find_packages(),
+    install_requires = ["requests", "mwparserfromhell"],
+)
