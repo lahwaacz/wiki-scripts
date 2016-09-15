25015e320d010b1882d7cf9ec789d82237dc6436
Jakub Klinkovský <j.l.k@gmx.com>
2016-09-15 23:18:06 +0200

    db: make URL parameters configurable

--- ws/db/database.py
+++ ws/db/database.py
@@ -13,6 +13,7 @@ Prerequisites:
 """
 
 from sqlalchemy import create_engine, Table, MetaData
+from sqlalchemy.engine import Engine
 
 from . import schema
 
@@ -22,11 +23,14 @@ class Database:
     charset = "utf8"
 
     # TODO: take parameters
-    def __init__(self):
+    def __init__(self, engine_or_url):
         # limit for continuation
         self.chunk_size = 5000
 
-        self.engine = create_engine("mysql+pymysql://wiki-scripts:wiki-scripts@localhost/wiki-scripts?charset={charset}".format(charset=self.charset), echo=True)
+        if isinstance(engine_or_url, Engine):
+            self.engine = engine_or_url
+        else:
+            self.engine = create_engine(engine_or_url, echo=True)
 
         # TODO: only for testing
         metadata = MetaData(bind=self.engine)
@@ -44,6 +48,59 @@ class Database:
         self.archive = self.metadata.tables["archive"]
         self.revision = self.metadata.tables["revision"]
         self.text = self.metadata.tables["text"]
+
+    @staticmethod
+    def make_url(username, password, host, database, **kwargs):
+        """
+        :param str username: username for database connection
+        :param str password: password for database connection
+        :param str host: hostname of the database server
+        :param str database: database name
+        :param dict kwargs: additional parameters added to the query string part
+        """
+        kwargs["charset"] = Database.charset
+        params = "&".join("{0}={1}".format(k, v) for k, v in kwargs.items())
+        return "mysql+pymysql://{username}:{password}@{host}/{database}?{params}" \
+               .format(username=username,
+                       password=password,
+                       host=host,
+                       database=database,
+                       params=params)
+
+    @staticmethod
+    def set_argparser(argparser):
+        """
+        Add arguments for constructing a :py:class:`Database` object to an
+        instance of :py:class:`argparse.ArgumentParser`.
+
+        See also the :py:mod:`ws.config` module.
+
+        :param argparser: an instance of :py:class:`argparse.ArgumentParser`
+        """
+        import ws.config
+        group = argparser.add_argument_group(title="Database parameters")
+        group.add_argument("--db-user", metavar="USER",
+                help="username for database connection (default: %(default)s)")
+        group.add_argument("--db-password", metavar="PASSWORD",
+                help="password for database connection (default: %(default)s)")
+        group.add_argument("--db-host", metavar="HOST",
+                help="hostname of the database server (default: %(default)s)")
+        group.add_argument("--db-name", metavar="DATABASE",
+                help="name of the database (default: %(default)s)")
+
+    @classmethod
+    def from_argparser(klass, args):
+        """
+        Construct a :py:class:`Database` object from arguments parsed by
+        :py:class:`argparse.ArgumentParser`.
+
+        :param args:
+            an instance of :py:class:`argparse.Namespace`. It is assumed that it
+            contains the arguments set by :py:meth:`Connection.set_argparser`.
+        :returns: an instance of :py:class:`Connection`
+        """
+        url = klass.make_url(args.db_user, args.db_password, args.db_host, args.db_name)
+        return klass(url)
 
 
 
