da3481f17dbf86b456fd6d62a1065460b4b15179
Jakub Klinkovský <j.l.k@gmx.com>
2016-09-18 19:10:16 +0200

    tests: update existing database tables

--- tests/test_db/test_db_create.py
+++ tests/test_db/test_db_create.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 
-custom_tables = {"namespace", "namespace_name"}
+custom_tables = {"namespace", "namespace_name", "ws_sync"}
+users_tables = {"user", "user_groups", "ipblocks"}
 pages_tables = {"archive", "category", "page", "page_props", "page_restrictions", "protected_titles", "redirect", "revision", "text"}
-all_tables = custom_tables | pages_tables
+all_tables = custom_tables | users_tables | pages_tables
 
 def test_db_create(db):
     tables = set(db.metadata.tables)
