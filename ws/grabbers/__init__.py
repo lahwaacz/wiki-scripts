490011159781545a7a76646d6845635965b7eb43
Jakub Klinkovský <j.l.k@gmx.com>
2016-09-19 19:11:18 +0200

    next iteration of an interface for grabbers - testing on page

---
+++ ws/grabbers/__init__.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python3
+
+import datetime
+
+from sqlalchemy import select
+
+from ws.client.api import ShortRecentChangesError
+
+class Grabber:
+
+    # class attributes that should be overridden in subclasses
+
+    # Names of tables that are managed by this grabber. Should be ordered such
+    # that INSERT statements in this order don't cause constraint errors.
+    TARGET_TABLES = []
+
+    def __init__(self, api, db):
+        self.api = api
+        self.db = db
+
+        if not self.TARGET_TABLES:
+            raise Exception("The {} class does not have TARGET_TABLES class attribute.".format(self.__class__.__name__))
+
+        # a mapping of ``(action, table)`` tuples to prepared SQL constructs
+        # (see self.gen_insert and self.gen_update for ``(action, table)`` tuples)
+        self.sql_constructs = {}
+
+    def _set_sync_timestamp(self, timestamp):
+        """
+        Set a last-sync timestamp for the grabber. Writes into the custom
+        ``ws_sync`` tyble.
+
+        :param datetime.datetime timestamp: the new timestamp
+        """
+        ws_sync = self.db.metadata.tables["ws_sync"]
+        ins = ws_sync.insert(mysql_on_duplicate_key_update=[ws_sync.c.wss_timestamp])
+        entry = {
+            "wss_table": self.__class__.__name__,
+            "wss_timestamp": timestamp,
+        }
+
+        with self.db.engine.begin() as conn:
+            conn.execute(ins, entry)
+
+    def _get_sync_timestamp(self):
+        """
+        Set a last-sync timestamp for the grabber. Reads from the custom
+        ``ws_sync`` tyble.
+        """
+        ws_sync = self.db.metadata.tables["ws_sync"]
+        sel = select([ws_sync.c.wss_timestamp]) \
+              .where(ws_sync.c.wss_table == self.__class__.__name__)
+
+        conn = self.db.engine.connect()
+        row = conn.execute(sel).fetchone()
+        if row:
+            return row[0]
+        return None
+
+    def gen_insert(self):
+        """
+        A generator for database entries which assumes that the tables are
+        empty.
+
+        To be implemented in subclasses.
+
+        The yielded entries should have this format:
+
+        - ``(action, table, entry)`` tuple, where ``action == "insert"``,
+          ``table`` is the target table and ``entry`` is a dict corresponding
+          to one row in ``table``. The SQL statement is constructed from
+          :py:attr:`self.sql_constructs` using the ``(action, table)``
+          tuple, entries are aggregated for the _executemany_ execution
+          strategy.
+        """
+        raise NotImplementedError
+
+    def gen_update(self, since):
+        """
+        A generator for database entries which assumes incremental updates.
+
+        To be implemented in subclasses.
+
+        If :py:exc:`ws.client.api.ShortRecentChangesError` is raised while
+        executing this generator, :py:meth:`self.update` starts from scratch
+        with :py:meth:`self.insert`. The exception should be raised as soon as
+        possible to save unnecessary work.
+
+        The yielded entries should have this format:
+
+        - ``(action, table, entry)`` tuple, where ``action`` is e.g. "insert",
+          ``table`` is the target table and ``entry`` is a dict corresponding
+          to one row in ``table``. The SQL statement is constructed from
+          :py:attr:`self.sql_constructs` using the ``(action, table)``
+          tuple, entries are aggregated for the _executemany_ execution
+          strategy.
+        - Or it can yield SQL statements directly, if the _executemany_
+          execution strategy is not applicable (e.g. for DELETE statements).
+        """
+        raise NotImplementedError
+
+    def insert(self):
+        # delete everything and start over, otherwise the invalid rows would
+        # stay in the tables
+        with self.db.engine.begin() as conn:
+            for table in self.TARGET_TABLES:
+                conn.execute(self.db.metadata.tables[table].delete())
+
+        sync_timestamp = datetime.datetime.utcnow()
+
+        gen = self.gen_insert()
+        self.db_execute(gen)
+
+        self._set_sync_timestamp(sync_timestamp)
+
+    def update(self):
+        sync_timestamp = datetime.datetime.utcnow()
+        since = self._get_sync_timestamp()
+        if since is None:
+            self.insert()
+            return
+
+        try:
+            gen = self.gen_update(since)
+            self.db_execute(gen)
+
+            self._set_sync_timestamp(sync_timestamp)
+        except ShortRecentChangesError:
+            logger.warning("The recent changes table on the wiki has been recently purged, starting from scratch.")
+            self.insert()
+            return
+
+    def db_execute(self, gen):
+        queues = {}
+
+        # execute all queues, ordered by TARGET_TABLES to avoid errors due to
+        # failing foreign keys constraints
+        def exec_all():
+            for table in self.TARGET_TABLES:
+                for action, table_ in queues.keys():
+                    if table == table_:
+                        queues[action, table].execute()
+
+        for item in gen:
+            if isinstance(item, tuple):
+                action, table, entry = item
+                queues.setdefault( (action, table), DbExecutionQueue(self.db, self.sql_constructs[action, table]) )
+                q = queues[action, table]
+                need_exec = q.add_entry(entry)
+                if need_exec:
+                    # execute all queues, even if some are (very) short - otherwise
+                    # they may get out-of-sync and cause constraint errors
+                    exec_all()
+            else:
+                with self.db.engine.begin() as conn:
+                    conn.execute(item)
+
+        # finalize queues
+        exec_all()
+
+
+class DbExecutionQueue:
+    def __init__(self, db, stmt):
+        self.db = db
+        self.stmt = stmt
+        self.entries = []
+
+    def add_entry(self, entry):
+        """
+        :returns: if the queue should be executed
+        """
+        self.entries.append(entry)
+        if len(self.entries) >= self.db.chunk_size:
+            return True
+        return False
+
+    def execute(self):
+        if self.entries:
+            with self.db.engine.begin() as conn:
+                conn.execute(self.stmt, self.entries)
+            self.entries = []
--- ws/grabbers/page.py
+++ ws/grabbers/page.py
@@ -1,247 +1,190 @@
 #!/usr/bin/env python3
 
-import datetime
 import random
+import logging
 
 import ws.utils
 from ws.parser_helpers.title import Title
+from ws.client.api import ShortRecentChangesError
 
-def gen_inserts_from_page(api, page):
-    title = Title(api, page["title"])
+from . import Grabber
 
-    # items for page table
-    db_entry = {
-        "page_id": page["pageid"],
-        "page_namespace": page["ns"],
-        # title is stored without the namespace prefix
-        "page_title": title.pagename,
-        "page_is_redirect": "redirect" in page,
-        "page_is_new": "new" in page,
-        "page_random": random.random(),
-        "page_touched": page["touched"],
-        "page_links_updated": None,
-        "page_latest": page["lastrevid"],
-        "page_len": page["length"],
-        "page_content_model": page["contentmodel"],
-        "page_lang": page["pagelanguage"],
-    }
-    yield "insert", db_entry
+logger = logging.getLogger(__name__)
 
-    # items for page_props table
-    for propname, value in page.get("pageprops", {}).items():
-        db_entry = {
-            "pp_page": page["pageid"],
-            "pp_propname": propname,
-            "pp_value": value,
-            # TODO: how should this be populated?
-#            "pp_sortkey": 
+class GrabberPages(Grabber):
+
+    TARGET_TABLES = ["page", "page_props", "page_restrictions"]
+
+    def __init__(self, api, db):
+        super().__init__(api, db)
+
+        self.sql_constructs = {
+            ("insert", "page"):
+                db.page.insert(mysql_on_duplicate_key_update=[
+                    db.page.c.page_namespace,
+                    db.page.c.page_title,
+                    db.page.c.page_is_redirect,
+                    db.page.c.page_is_new,
+                    db.page.c.page_random,
+                    db.page.c.page_touched,
+                    db.page.c.page_links_updated,
+                    db.page.c.page_latest,
+                    db.page.c.page_len,
+                    db.page.c.page_content_model,
+                    db.page.c.page_lang,
+                ]),
+            ("insert", "page_props"):
+                db.page_props.insert(mysql_on_duplicate_key_update=[
+                    db.page_props.c.pp_value,
+                ]),
+            ("insert", "page_restrictions"):
+                db.page_restrictions.insert(mysql_on_duplicate_key_update=[
+                    db.page_restrictions.c.pr_level,
+                    db.page_restrictions.c.pr_cascade,
+                    db.page_restrictions.c.pr_user,
+                    db.page_restrictions.c.pr_expiry,
+                ]),
+            ("delete", "page"):
+                db.page.delete().where(db.page.c.page_id == "?"),
+            ("delete!", "page_props"):
+                db.page_props.delete().where(
+                    db.page_props.c.pp_page == "?" and
+                    db.page_props.c.pp_propname.notin_("?")),
+            ("delete!", "page_restrictions"):
+                db.page_restrictions.delete().where(
+                    db.page_restrictions.c.pr_page == "?" and
+                    db.page_restrictions.c.pr_type.notin_("?")),
         }
-        yield "insert", db_entry
 
-    # items for page_restrictions table
-    for pr in page["protection"]:
-        # drop entries caused by cascading protection
-        if "source" not in pr:
+
+    def gen_inserts_from_page(self, page):
+        if "missing" in page:
+            raise StopIteration
+
+        title = Title(self.api, page["title"])
+
+        # items for page table
+        db_entry = {
+            "page_id": page["pageid"],
+            "page_namespace": page["ns"],
+            # title is stored without the namespace prefix
+            "page_title": title.pagename,
+            "page_is_redirect": "redirect" in page,
+            "page_is_new": "new" in page,
+            "page_random": random.random(),
+            "page_touched": page["touched"],
+            "page_links_updated": None,
+            "page_latest": page["lastrevid"],
+            "page_len": page["length"],
+            "page_content_model": page["contentmodel"],
+            "page_lang": page["pagelanguage"],
+        }
+        yield "insert", "page", db_entry
+
+        # items for page_props table
+        for propname, value in page.get("pageprops", {}).items():
             db_entry = {
-                "pr_page": page["pageid"],
-                "pr_type": pr["type"],
-                "pr_level": pr["level"],
-                "pr_cascade": "cascade" in pr,
-                "pr_user": None,    # unused
-                "pr_expiry": pr["expiry"],
+                "pp_page": page["pageid"],
+                "pp_propname": propname,
+                "pp_value": value,
+                # TODO: how should this be populated?
+#                "pp_sortkey":
             }
-            yield "insert", db_entry
+            yield "insert", "page_props", db_entry
+
+        # items for page_restrictions table
+        for pr in page["protection"]:
+            # drop entries caused by cascading protection
+            if "source" not in pr:
+                db_entry = {
+                    "pr_page": page["pageid"],
+                    "pr_type": pr["type"],
+                    "pr_level": pr["level"],
+                    "pr_cascade": "cascade" in pr,
+                    "pr_user": None,    # unused
+                    "pr_expiry": pr["expiry"],
+                }
+                yield "insert", "page_restrictions", db_entry
 
 
-def gen_deletes_from_page(api, page):
-    if "missing" in page:
-        # deleted page - this will cause cascade deletion in
-        # page_props and page_restrictions tables
-        yield "delete-page", page["pageid"]
-    else:
-        # delete outdated props
-        props = set(page.get("pageprops", {}))
-        yield "delete-pp!", (page["pageid"], props)
+    def gen_deletes_from_page(self, page):
+        if "missing" in page:
+            # deleted page - this will cause cascade deletion in
+            # page_props and page_restrictions tables
+            yield "delete", "page", page["pageid"]
+        else:
+            # delete outdated props
+            props = set(page.get("pageprops", {}))
+            yield "delete!", "page_props", (page["pageid"], props)
 
-        # delete outdated restrictions
-        applied = set(pr["type"] for pr in page["protection"])
-        yield "delete-pr!", (page["pageid"], applied)
-
-
-def gen_insert(api):
-    params = {
-        "generator": "allpages",
-        "gaplimit": "max",
-        "prop": "info|pageprops",
-        "inprop": "protection",
-    }
-    for ns in api.site.namespaces.keys():
-        if ns < 0:
-            continue
-        params["gapnamespace"] = ns
-        for page in api.generator(params):
-            yield from gen_inserts_from_page(api, page)
+            # delete outdated restrictions
+            applied = set(pr["type"] for pr in page["protection"])
+            yield "delete!", "page_restrictions", (page["pageid"], applied)
 
 
-def gen_update(api, rcpages):
-    logger.info("Fetching properties of {} modified pages...".format(len(rcpages)))
-    for chunk in ws.utils.iter_chunks(rcusers, api.max_ids_per_query):
+    def gen_insert(self):
         params = {
-            "action": "query",
-            "pageids": "|".join(chunk),
+            "generator": "allpages",
+            "gaplimit": "max",
             "prop": "info|pageprops",
             "inprop": "protection",
         }
-        for page in gen(api, params):
-            yield from gen_inserts_from_page(api, page)
-            yield from gen_deletes_from_page(api, page)
+        for ns in self.api.site.namespaces.keys():
+            if ns < 0:
+                continue
+            params["gapnamespace"] = ns
+            for page in self.api.generator(params):
+                yield from self.gen_inserts_from_page(page)
 
 
-def gen_rcpages(api, since):
-    since_f = ws.utils.format_date(since)
-    rcpages = set()
-
-    # Items in the recentchanges table are periodically purged according to
-    # http://www.mediawiki.org/wiki/Manual:$wgRCMaxAge
-    # By default the max age is 13 weeks: if a larger timespan is requested
-    # here, it's very important to warn that the changes are not available
-    if api.oldest_recent_change > since:
-        raise ShortRecentChangesError()
-
-    rc_params = {
-        "action": "query",
-        "list": "recentchanges",
-        "rctype": "edit|new|log",
-        "rcprop": "ids",
-        "rclimit": "max",
-        "rcdir": "newer",
-        "rcstart": since_f,
-    }
-    for change in api.list(rc_params):
-        # add pageid for edits, new pages and target pages of log events
-        rcpages.add(change["pageid"])
-
-        # TODO: examine logs (needs rcprop=loginfo)
-        # move, protect, delete are handled by the above
-        # these deserve special treatment
-        #   merge       (revision - or maybe page too?)
-        #   import      (everything?)
-        #   patrol      (page)  (not in recentchanges! so we can't know when a page loses its 'new' flag)
-        #   suppress    (everything?)
-#        if change["type"] == "log":
-#            if change["logtype"] == "merge":
-#                ...
-
-    return rcpages
+    def gen_update(self, since):
+        rcpages = self.get_rcpages(since)
+        logger.info("Fetching properties of {} modified pages...".format(len(rcpages)))
+        for chunk in ws.utils.iter_chunks(rcpages, self.api.max_ids_per_query):
+            params = {
+                "action": "query",
+                "pageids": "|".join(str(pageid) for pageid in chunk),
+                "prop": "info|pageprops",
+                "inprop": "protection",
+            }
+            for page in self.api.call_api(params)["pages"].values():
+                yield from self.gen_inserts_from_page(page)
+                yield from self.gen_deletes_from_page(page)
 
 
-def db_execute(db, gen):
-    page_ins = db.page.insert(mysql_on_duplicate_key_update=[
-                                    db.page.c.page_namespace,
-                                    db.page.c.page_title,
-                                    db.page.c.page_is_redirect,
-                                    db.page.c.page_is_new,
-                                    db.page.c.page_random,
-                                    db.page.c.page_touched,
-                                    db.page.c.page_links_updated,
-                                    db.page.c.page_latest,
-                                    db.page.c.page_len,
-                                    db.page.c.page_content_model,
-                                    db.page.c.page_lang,
-                                ])
-    pp_ins = db.page_props.insert(mysql_on_duplicate_key_update=[
-                                    db.page_props.c.pp_value,
-                                ])
-    pr_ins = db.page_restrictions.insert(mysql_on_duplicate_key_update=[
-                                    db.page_restrictions.c.pr_level,
-                                    db.page_restrictions.c.pr_cascade,
-                                    db.page_restrictions.c.pr_user,
-                                    db.page_restrictions.c.pr_expiry,
-                                ])
+    def get_rcpages(self, since):
+        since_f = ws.utils.format_date(since)
+        rcpages = set()
 
-    for chunk in ws.utils.iter_chunks(gen, db.chunk_size):
-        # separate according to action and target table
-        page_ins_entries = []
-        pp_ins_entries = []
-        pr_ins_entries = []
-        page_del_pageids = []
-        pp_invdel_entries = []
-        pr_invdel_entries = []
+        # Items in the recentchanges table are periodically purged according to
+        # http://www.mediawiki.org/wiki/Manual:$wgRCMaxAge
+        # By default the max age is 13 weeks: if a larger timespan is requested
+        # here, it's very important to warn that the changes are not available
+        if self.api.oldest_recent_change > since:
+            raise ShortRecentChangesError()
 
-        for action, entry in chunk:
-            if action == "insert":
-                if "page_id" in entry:
-                    page_ins_entries.append(entry)
-                elif "pp_page" in entry:
-                    pp_ins_entries.append(entry)
-                elif "pr_page" in entry:
-                    pr_ins_entries.append(entry)
-                else:  # pragma: no cover
-                    raise Exception
-            elif action == "delete-page":
-                page_del_pageids.append(entry)
-            elif action == "delete-pp!":
-                pp_invdel_entries.append(entry)
-            elif action == "delete-pr!":
-                pr_invdel_entries.append(entry)
-            else:  # pragma: no cover
-                raise Exception
+        rc_params = {
+            "action": "query",
+            "list": "recentchanges",
+            "rctype": "edit|new|log",
+            "rcprop": "ids",
+            "rclimit": "max",
+            "rcdir": "newer",
+            "rcstart": since_f,
+        }
+        for change in self.api.list(rc_params):
+            # add pageid for edits, new pages and target pages of log events
+            rcpages.add(change["pageid"])
 
+            # TODO: examine logs (needs rcprop=loginfo)
+            # move, protect, delete are handled by the above
+            # these deserve special treatment
+            #   merge       (revision - or maybe page too?)
+            #   import      (everything?)
+            #   patrol      (page)  (not in recentchanges! so we can't know when a page loses its 'new' flag)
+            #   suppress    (everything?)
+#            if change["type"] == "log":
+#                if change["logtype"] == "merge":
+#                    ...
 
-        with db.engine.begin() as conn:
-            if page_ins_entries:
-                conn.execute(page_ins, page_ins_entries)
-            if pp_ins_entries:
-                conn.execute(pp_ins, pp_ins_entries)
-            if pr_ins_entries:
-                conn.execute(pr_ins, pr_ins_entries)
-
-            if page_del_pageids:
-                conn.execute(db.page.delete().where(page.c.page_id in page_del_pageids))
-
-            for pageid, props in pp_invdel_entries:
-                conn.execute(db.page_props.delete().where(
-                                page_props.c.pr_page == pageid and \
-                                page_props.c.pr_propname not in props))
-
-            for pageid, applied_prs in pr_invdel_entries:
-                conn.execute(db.page_restrictions.delete().where(
-                                page_restrictions.c.pr_page == pageid and \
-                                page_restrictions.c.pr_type not in applied_prs))
-
-
-def insert(api, db):
-    # delete everything and start over, otherwise the invalid rows would stay
-    # in the tables
-    with db.engine.begin() as conn:
-        conn.execute(db.page.delete())
-        conn.execute(db.page_props.delete())
-        conn.execute(db.page_restrictions.delete())
-
-    sync_timestamp = datetime.datetime.utcnow()
-
-    gen = gen_insert(api)
-    db_execute(db, gen)
-
-    db.set_sync_timestamp(db.page, sync_timestamp)
-
-
-def update(api, db):
-    sync_timestamp = datetime.datetime.utcnow()
-    since = db.get_sync_timestamp(db.page)
-    if since is None:
-        insert(api, db)
-        return
-
-    try:
-        rcpages = gen_rcpages(api, since)
-    except ShortRecentChangesError:
-        logger.warning("The recent changes table on the wiki has been recently purged, starting from scratch.")
-        insert(api, db)
-        return
-
-    if rcpages:
-        gen = gen_update(api, rcusers)
-        db_execute(db, gen)
-
-        db.set_sync_timestamp(db.page, sync_timestamp)
+        return rcpages
