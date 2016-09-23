6738c98f51c15741d17f7cd438604a6bcadbc4dd
Jakub Klinkovský <j.l.k@gmx.com>
2016-09-23 11:50:56 +0200

    tests: update sets of created tables

--- tests/test_db/test_db_create.py
+++ tests/test_db/test_db_create.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
 
-custom_tables = {"namespace", "namespace_name", "ws_sync"}
+custom_tables = {"namespace", "namespace_name", "namespace_starname", "namespace_canonical", "ws_sync"}
 users_tables = {"user", "user_groups", "ipblocks"}
-pages_tables = {"archive", "category", "page", "page_props", "page_restrictions", "protected_titles", "redirect", "revision", "text"}
-all_tables = custom_tables | users_tables | pages_tables
+pages_tables = {"archive", "page", "page_props", "page_restrictions", "protected_titles", "revision", "text"}
+recentchanges_tables = {"recentchanges", "watchlist"}
+all_tables = custom_tables | users_tables | pages_tables | recentchanges_tables
 
 def test_db_create(db):
     tables = set(db.metadata.tables)
