3f83b0fd0f66016da726757a1ebdbf9fceca62cd
Jakub Klinkovský <j.l.k@gmx.com>
2017-04-14 16:10:45 +0200

    db.grabbers: rewrite fetching revisions using list=allrevisions

--- ws/db/grabbers/revision.py
+++ ws/db/grabbers/revision.py
@@ -1,130 +1,170 @@
 #!/usr/bin/env python3
 
-from pprint import pprint
+import logging
 
-import itertools
 import sqlalchemy
-from sqlalchemy import bindparam
 from sqlalchemy.sql import func
 
 import ws.utils
+from ws.utils import format_date, value_or_none
+from ws.parser_helpers.title import Title
 
-# this should be equal to the API's limit for the revids= parameter
-#chunk_size_small = 500
-# ...but must be less in order to not trip the PHP size limit
-chunk_size_small = 100
+from . import Grabber
 
-# TODO: got many truncated results, should wait for list=allrevisions
-def gen(api, first, last, with_content=False, text_id_gen=None):
-    _props = "ids|timestamp|flags|user|userid|comment|size|sha1|contentmodel"
-    if with_content is True:
-        _props += "|content"
-    for chunk in ws.utils.list_chunks(range(first, last+1), chunk_size_small):
-        revids = "|".join(str(x) for x in chunk)
-        for result in api.query_continue(action="query", revids=revids, prop="info|revisions|deletedrevisions", drvlimit="max", rvprop=_props, drvprop=_props):
-            for _, page in result.get("pages", {}).items():
-                revisions = itertools.chain(page.get("revisions", []), page.get("deletedrevisions", []))
-                for rev in revisions:
-                    db_entry = {
-                        "rev_id": rev["revid"],
-                        "rev_page": page.get("pageid"),
-                        "rev_comment": rev["comment"],
-                        "rev_user": rev["userid"],
-                        "rev_user_text": rev["user"],
-                        "rev_timestamp": rev["timestamp"],
-                        "rev_minor_edit": "minor" in rev,
-                        # TODO: rev_deleted
-                        "rev_len": rev["size"],
-                        # TODO: read on page history merging
-                        "rev_parent_id": rev.get("parentid"),
-                        "rev_sha1": rev["sha1"],
-                        "rev_content_model": rev["contentmodel"],
-                        # bound parameter used in update queries
-                        "b_rev_id": rev["revid"],
-                    }
-                    if with_content is True:
-                        text_id = next(text_id_gen)
-                        db_entry.update({
-                            "rev_text_id": text_id,
-                            "old_id": text_id,
-                            "old_text": rev["*"],
-                            # TODO: possibility for compression
-                            "old_flags": "utf-8",
-                        })
-                    yield db_entry
+logger = logging.getLogger(__name__)
 
-# TODO: text.old_id is auto-increment, but revision.rev_text_id has to be set accordingly. SQL should be able to do it automatically.
-def _get_text_id(conn):
-    result = conn.execute(sqlalchemy.select( [func.max(db.text.c.old_id)] ))
-    value = result.fetchone()[0]
-    if value is None:
-        value = 0
-    while True:
-        value += 1
-        yield value
+# TODO: allow syncing content independently from revisions
+# TODO: are truncated results due to PHP cache reflected by changing the query-continuation parameter accordingly or do we actually lose some revisions?
+class GrabberRevisions(Grabber):
 
-def _get_last_revid_db(conn):
-    """
-    Get ID of the last revision stored in the cache database.
-    """
-    result = conn.execute(sqlalchemy.select( [func.max(db.revision.c.rev_id)] ))
-    value = result.fetchone()[0]
-    if value is None:
-        return 0
-    return value
+    def __init__(self, api, db, with_content=False):
+        super().__init__(api, db)
+        self.with_content = with_content
 
-def insert(api, db):
-    revision_ins = db.revision.insert()
-    text_ins = db.text.insert()
-
-    conn = db.engine.connect()
-
-    # get revision IDs
-    firstrevid = 1
-    midrevid = _get_last_revid_db(conn)
-    lastrevid = api.last_revision_id
-
-    text_id_gen = _get_text_id(conn)
-
-    # update existing revisions
-    # TODO: optimize, necessary only to update rev_page_id after history merging
-    for chunk in ws.utils.iter_chunks(gen(api, firstrevid, midrevid), db.chunk_size):
-        entries = list(chunk)
-        conn.execute(revision_upd, entries)
-
-    # insert new revisions
-    for chunk in ws.utils.iter_chunks(gen(api, midrevid + 1, lastrevid, with_content=True, text_id_gen=text_id_gen), db.chunk_size):
-        entries = list(chunk)
-        conn.execute(revision_ins, entries)
-        conn.execute(text_ins, entries)
-
-def update(api, db):
-    revision_upd = db.revision.update().where(db.revision.c.rev_id == bindparam("b_rev_id"))
-    raise NotImplementedError
-
-def select(db):
-    # TODO: reconstructing the API entries will be hard, after all the data were fetched with a generator
-#    raise NotImplementedError
-
-    conn = db.engine.connect()
-    s = sqlalchemy.select([db.revision, db.text.c.old_text]).where(
-            db.revision.c.rev_text_id == db.text.c.old_id
-        )
-    for row in conn.execute(s):
-        api_revision_entry = {
-            "revid": row.rev_id,
-            "comment": row.rev_comment,
-            "userid": row.rev_user,
-            "user": row.rev_user_text,
-            "timestamp": row.rev_timestamp,
-            # TODO: rev_deleted
-            "size": row.rev_len,
-            "parentid": row.rev_parent_id,
-            "sha1": row.rev_sha1,
-            "contentmodel": row.rev_content_model,
-            "*": row.old_text,
+        self.sql = {
+            ("insert", "text"):
+                db.text.insert(
+                    on_conflict_constraint=[db.text.c.old_id],
+                    on_conflict_update=[
+                        db.text.c.old_text,
+                        db.text.c.old_flags,
+                    ]),
+            ("insert", "revision"):
+                db.revision.insert(
+                    on_conflict_constraint=[db.revision.c.rev_id],
+                    on_conflict_update=[
+                        # this should be the only columns that may change in the table
+                        db.revision.c.rev_deleted,
+                        # TODO: merging might change rev_page and rev_parent_id
+                    ]),
+            ("insert", "archive"):
+                db.archive.insert(
+                    on_conflict_constraint=[db.archive.c.ar_rev_id],
+                    on_conflict_update=[
+                        # this should be the only columns that may change in the table
+                        db.archive.c.ar_deleted,
+                        # TODO: merging might change ar_page_id and rev_parent_id
+                    ]),
         }
-        if row.rev_minor_edit:
-            api_revision_entry["minor"] = ""
 
-        pprint(api_revision_entry)
+        props = "ids|timestamp|flags|user|userid|comment|size|sha1|contentmodel"
+        if self.with_content is True:
+            props += "|content"
+
+        # TODO: tags
+        self.arv_params = {
+            "list": "allrevisions",
+            "arvprop": props,
+            "arvlimit": "max",
+        }
+
+        # TODO: tags
+        self.adr_params = {
+            "list": "alldeletedrevisions",
+            "adrprop": props,
+            "adrlimit": "max",
+        }
+
+        # TODO: check the permission to view deleted revisions
+#        if "patrol" in self.api.user.rights:
+#            self.rc_params["rcprop"] += "|patrolled"
+#        else:
+#            logger.warning("You need the 'patrol' right to request the patrolled flag. "
+#                           "Skipping it, but the sync will be incomplete.")
+
+    # TODO: text.old_id is auto-increment, but revision.rev_text_id has to be set accordingly. SQL should be able to do it automatically.
+    def _get_text_id(self, conn):
+        result = conn.execute(sqlalchemy.select( [func.max(self.db.text.c.old_id)] ))
+        value = result.fetchone()[0]
+        if value is None:
+            value = 0
+        while True:
+            value += 1
+            yield value
+
+    def gen_text(self, rev, text_id):
+        db_entry = {
+            "old_id": text_id,
+            "old_text": rev["*"],
+            "old_flags": "utf-8",
+        }
+        yield self.sql["insert", "text"], db_entry
+
+    def gen_revisions(self, page):
+        for rev in page["revisions"]:
+            db_entry = {
+                "rev_id": rev["revid"],
+                "rev_page": value_or_none(page.get("pageid")),
+                "rev_comment": rev["comment"],
+                "rev_user": rev["userid"],
+                "rev_user_text": rev["user"],
+                "rev_timestamp": rev["timestamp"],
+                "rev_minor_edit": "minor" in rev,
+                # TODO: rev_deleted
+                "rev_len": rev["size"],
+                # TODO: read on page history merging
+                "rev_parent_id": rev.get("parentid"),
+                "rev_sha1": rev["sha1"],
+                "rev_content_model": rev["contentmodel"],
+            }
+
+            if self.with_content is True:
+                text_id = next(text_id_gen)
+                db_entry["rev_text_id"] = text_id
+                yield from self.gen_text(rev, text_id)
+
+            yield self.sql["insert", "revision"], db_entry
+
+    def gen_deletedrevisions(self, page):
+        title = Title(self.api, page["title"])
+        for rev in page["revisions"]:
+            db_entry = {
+                "ar_namespace": page["ns"],
+                "ar_title": title.dbtitle(page["ns"]),
+                "ar_rev_id": rev["revid"],
+                "ar_page_id": value_or_none(page["pageid"]),
+                "ar_comment": rev["comment"],
+                "ar_user": rev["userid"],
+                "ar_user_text": rev["user"],
+                "ar_timestamp": rev["timestamp"],
+                "ar_minor_edit": "minor" in rev,
+                # TODO: ar_deleted
+                "ar_len": rev["size"],
+                # ar_parent_id is not visible through API
+                "ar_sha1": rev["sha1"],
+                "ar_content_model": rev["contentmodel"],
+            }
+
+            if self.with_content is True:
+                text_id = next(text_id_gen)
+                db_entry["rev_text_id"] = text_id
+                yield from self.gen_text(rev, text_id)
+
+            yield self.sql["insert", "archive"], db_entry
+
+    # TODO: write custom insert and update methods, use discontinued API queries and wrap each chunk in a separate transaction
+    # TODO: generalize the above even for logging table
+
+    def gen_insert(self):
+        for page in self.api.list(self.arv_params):
+            yield from self.gen_revisions(page)
+        for page in self.api.list(self.adr_params):
+            yield from self.gen_deletedrevisions(page)
+
+    def gen_update(self, since):
+        since_f = format_date(since)
+
+        arv_params = self.arv_params.copy()
+        arv_params["arvdir"] = "newer"
+        arv_params["arvstart"] = since_f
+        for page in self.api.list(arv_params):
+            yield from self.gen_revisions(page)
+
+# FIXME: MW defect: adrstart can be used only along with adruser
+#        adr_params = self.adr_params.copy()
+#        adr_params["adrdir"] = "newer"
+#        adr_params["adrstart"] = since_f
+#        for page in self.api.list(adr_params):
+#            yield from self.gen_deletedrevisions(page)
+
+        # TODO: go through the new logevents and update changed revisions (delete, merge)
