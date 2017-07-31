d60fc4a5fa17c2082b1f5e3150b46a37b9d7382d
Jakub Klinkovský <j.l.k@gmx.com>
2017-07-31 23:45:28 +0200

    update database tests

--- tests/test_db/test_db_create.py
+++ tests/test_db/test_db_create.py
@@ -3,7 +3,7 @@
 custom_tables = {"namespace", "namespace_name", "namespace_starname", "namespace_canonical", "ws_sync"}
 users_tables = {"user", "user_groups", "ipblocks"}
 pages_tables = {"archive", "page", "page_props", "page_restrictions", "protected_titles", "revision", "text"}
-recentchanges_tables = {"recentchanges", "watchlist"}
+recentchanges_tables = {"recentchanges", "logging"}
 all_tables = custom_tables | users_tables | pages_tables | recentchanges_tables
 
 def test_db_create(db):
--- tests/test_db/test_grabbers.py
+++ tests/test_db/test_grabbers.py
@@ -2,7 +2,8 @@
 
 from copy import copy
 
-from ws.grabbers import namespace
+from ws.db.grabbers import namespace, recentchanges
+import ws.db.selects.recentchanges as rcsel
 
 # TODO: monkeypatch api.site.namespaces to avoid API queries
 
@@ -19,3 +20,31 @@ def test_namespace(api, db):
     g = namespace.GrabberNamespaces(api, db)
     g.update()
     assert namespace.select(db) == ns
+
+# FIXME: requires authentication to the API, otherwise we don't get past "userhidden" etc.
+def test_recentchanges(api, db):
+    g = namespace.GrabberNamespaces(api, db)
+    g.update()
+    g = recentchanges.GrabberRecentChanges(api, db)
+    g.update()
+
+    # all supported except "patrolled", which is not public
+    prop = {"title", "ids", "user", "userid", "flags", "timestamp", "comment", "sizes", "loginfo", "sha1"}
+    api_params = {
+        "list": "recentchanges",
+        "rcprop": "|".join(prop),
+        "rclimit": "max",
+    }
+
+    api_list = list(api.list(api_params))
+    db_list = list(rcsel.list(db, prop=prop))
+
+    # don't assert the whole lists, otherwise the diff engine won't finish in finite time
+    assert len(db_list) == len(api_list)
+    for i, entries in enumerate(zip(db_list, api_list)):
+        db_entry, api_entry = entries
+        # sha1 is needed to unclude the "sha1hidden" flag on deleted revisions,
+        # but we haven't synced the revision table yet
+        if "sha1" in api_entry:
+            del api_entry["sha1"]
+        assert db_entry == api_entry
