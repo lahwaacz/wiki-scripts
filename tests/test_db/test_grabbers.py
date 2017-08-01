959c5029f35cedbec748c74af49684500cf38270
Jakub Klinkovský <j.l.k@gmx.com>
2017-08-01 09:16:44 +0200

    tests: fix test_recentchanges

--- tests/test_db/test_grabbers.py
+++ tests/test_db/test_grabbers.py
@@ -21,7 +21,6 @@ def test_namespace(api, db):
     g.update()
     assert namespace.select(db) == ns
 
-# FIXME: requires authentication to the API, otherwise we don't get past "userhidden" etc.
 def test_recentchanges(api, db):
     g = namespace.GrabberNamespaces(api, db)
     g.update()
@@ -39,12 +38,21 @@ def test_recentchanges(api, db):
     api_list = list(api.list(api_params))
     db_list = list(rcsel.list(db, prop=prop))
 
+    # hack to get past "userhidden" etc. with anonymous connection to the API
+    skip_deleted = "deleterevision" not in api.user.rights
+
     # don't assert the whole lists, otherwise the diff engine won't finish in finite time
     assert len(db_list) == len(api_list)
     for i, entries in enumerate(zip(db_list, api_list)):
         db_entry, api_entry = entries
-        # sha1 is needed to unclude the "sha1hidden" flag on deleted revisions,
+        # sha1 is needed to include the "sha1hidden" flag on deleted revisions,
         # but we haven't synced the revision table yet
         if "sha1" in api_entry:
             del api_entry["sha1"]
+
+        if skip_deleted:
+            # TODO: skip also "userid" and "user" when needed
+            if "commenthidden" in db_entry:
+                del db_entry["comment"]
+
         assert db_entry == api_entry
