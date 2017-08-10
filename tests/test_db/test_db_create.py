ffaac73fd3724bfa754364d2570123e2cd6acf8e
Jakub Klinkovský <j.l.k@gmx.com>
2017-08-10 10:57:59 +0200

    tests: update tables in test_db_create

--- tests/test_db/test_db_create.py
+++ tests/test_db/test_db_create.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
 
 custom_tables = {"namespace", "namespace_name", "namespace_starname", "namespace_canonical", "ws_sync"}
+site_tables = {"interwiki", "tag"}
+recentchanges_tables = {"recentchanges", "logging", "tagged_recentchange", "tagged_logevent"}
 users_tables = {"user", "user_groups", "ipblocks"}
-pages_tables = {"archive", "page", "page_props", "page_restrictions", "protected_titles", "revision", "text"}
-recentchanges_tables = {"recentchanges", "logging"}
-all_tables = custom_tables | users_tables | pages_tables | recentchanges_tables
+revisions_tables = {"archive", "revision", "text", "tagged_revision", "tagged_archived_revision"}
+pages_tables = {"page", "page_props", "page_restrictions", "protected_titles"}
+all_tables = custom_tables | site_tables| recentchanges_tables | users_tables | revisions_tables | pages_tables
 
 def test_db_create(db):
     tables = set(db.metadata.tables)
