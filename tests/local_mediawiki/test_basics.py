94d367e3caa702156d06a43b9619284610516ba1
Jakub Klinkovský <j.l.k@gmx.com>
2017-08-13 23:40:44 +0200

    tests: assert rights on local mediawiki install

--- tests/local_mediawiki/test_basics.py
+++ tests/local_mediawiki/test_basics.py
@@ -9,8 +9,29 @@ def test_server_root(mw_server_root):
     assert os.path.isfile(os.path.join(mw_server_root, "index.php"))
     assert os.path.isfile(os.path.join(mw_server_root, "load.php"))
 
-def test_mediawiki(mediawiki):
+def test_mw_api(mediawiki):
     api = mediawiki.api
-    db_engine = mediawiki.db_engine
     assert api.user.is_loggedin
     assert "sysop" in api.user.groups
+
+    expected_rights = {
+        "createpage",
+        "createtalk",
+        "writeapi",
+        "apihighlimits",
+        "noratelimit",
+        "interwiki",
+        "delete",
+        "bigdelete",
+        "deleterevision",
+        "deletelogentry",
+        "deletedhistory",
+        "deletedtext",
+        "browsearchive",
+        "mergehistory",
+        "autopatrol",
+        "patrol",
+    }
+    # pytest's assertion does not show diff for subset checks...
+    for right in expected_rights:
+        assert right in api.user.rights
