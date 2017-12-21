7bb23e9e5d09ca3df380aebbe9e755dbd1e0e951
Jakub Klinkovský <j.l.k@gmx.com>
2017-12-21 18:23:11 +0100

    db: make ar_page_id zeroable in the alldeletedrevisions select

--- ws/db/selects/alldeletedrevisions.py
+++ ws/db/selects/alldeletedrevisions.py
@@ -161,7 +161,7 @@ def db_to_api(row):
         "ar_minor_edit": "minor",
     }
     # subset of flags for which 0 should be used instead of None
-    zeroable_flags = {"ar_user", "ar_parent_id"}
+    zeroable_flags = {"ar_user", "ar_parent_id", "ar_page_id"}
 
     api_entry = {}
     for key, value in row.items():
