6f79cc0a9db960beed6e15dc02b9643d7ee9a13b
Jakub Klinkovský <j.l.k@gmx.com>
2017-12-23 00:30:30 +0100

    remove old tests

--- tests/test_db/test_grabbers.py
+++ tests/test_db/test_grabbers.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 from copy import copy
-import datetime
 
 import pytest
 
@@ -9,14 +8,9 @@ import ws.db.grabbers as grabbers
 import ws.db.grabbers.namespace
 import ws.db.grabbers.tags
 import ws.db.grabbers.recentchanges
-import ws.db.grabbers.user
-import ws.db.grabbers.logging
-import ws.db.grabbers.page
 
 import ws.db.selects as selects
 import ws.db.selects.recentchanges
-import ws.db.selects.logevents
-import ws.db.selects.allpages
 
 # TODO: monkeypatch api.site.namespaces to avoid API queries
 
@@ -72,66 +66,4 @@ def test_recentchanges(api, db):
             if "commenthidden" in db_entry:
                 del db_entry["comment"]
 
-        assert db_entry == api_entry
-
-@pytest.mark.skip(reason="useless test, needs to be migrated to python-bdd")
-def test_logging(api, db):
-    # fixed timestamps for reproducible and fast tests
-    now = datetime.datetime.utcnow()
-    since = now - datetime.timedelta(days=100)
-
-    g = grabbers.namespace.GrabberNamespaces(api, db)
-    g.update()
-    g = grabbers.tags.GrabberTags(api, db)
-    g.update()
-    g = grabbers.user.GrabberUsers(api, db)
-    g.update()
-    g = grabbers.logging.GrabberLogging(api, db)
-    # fixed timestamp skips init with g.insert(), which is fine for logging
-    g.update(since=since)
-    g = grabbers.page.GrabberPages(api, db)
-    g.update()
-
-    prop = {"user", "userid", "comment", "timestamp", "title", "ids", "type", "details"}
-    api_params = {
-        "list": "logevents",
-        "leprop": "|".join(prop),
-        "lelimit": "max",
-        "ledir": "newer",
-        "lestart": since,
-        "leend": now,
-    }
-
-    api_list = list(api.list(api_params))
-    db_list = list(selects.logevents.list(db, prop=prop, dir="newer", start=since, end=now))
-
-    # don't assert the whole lists, otherwise the diff engine won't finish in finite time
-    assert len(db_list) == len(api_list)
-    for i, entries in enumerate(zip(db_list, api_list)):
-        db_entry, api_entry = entries
-        assert db_entry == api_entry
-
-@pytest.mark.skip(reason="useless test, needs to be migrated to python-bdd")
-def test_allpages(api, db):
-    g = grabbers.namespace.GrabberNamespaces(api, db)
-    g.update()
-    g = grabbers.page.GrabberPages(api, db)
-    g.update()
-
-    api_params = {
-        "list": "allpages",
-        "aplimit": "max",
-    }
-
-    api_list = list(api.list(api_params))
-    db_list = list(selects.allpages.list(db))
-
-    # FIXME: hack around the unknown remote collation
-    api_list.sort(key=lambda item: item["pageid"])
-    db_list.sort(key=lambda item: item["pageid"])
-
-    print("Checking the page table...")
-    assert len(db_list) == len(api_list)
-    for i, entries in enumerate(zip(db_list, api_list)):
-        db_entry, api_entry = entries
         assert db_entry == api_entry
