dbccfdd4e705ba543ccc5dd76113be1d92d20102
Jakub Klinkovský <j.l.k@gmx.com>
2018-08-05 17:03:19 +0200

    db: use logger for synchronization status message

--- ws/db/grabbers/__init__.py
+++ ws/db/grabbers/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import logging
+
 from ws.db.grabbers.namespace import GrabberNamespaces
 from ws.db.grabbers.tags import GrabberTags
 from ws.db.grabbers.interwiki import GrabberInterwiki
@@ -9,13 +11,15 @@ from ws.db.grabbers.ipblocks import GrabberIPBlocks
 from ws.db.grabbers.page import GrabberPages
 from ws.db.grabbers.protected_titles import GrabberProtectedTitles
 from ws.db.grabbers.revision import GrabberRevisions
-from ws.db.grabbers.logging import GrabberLogging
+from ws.db.grabbers.logging_ import GrabberLogging
+
+logger = logging.getLogger(__name__)
 
 def synchronize(db, api, *, with_content=False):
     # if no recent change has been added, it's safe to assume that the other tables are up to date as well
     g = GrabberRecentChanges(api, db)
     if g.needs_update() is False:
-        print("No changes are needed according to the recentchanges table.")
+        logger.info("No new changes since the last database synchronization.")
         return
 
     GrabberNamespaces(api, db).update()
