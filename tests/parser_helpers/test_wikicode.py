813bf332e86be570ee500bb0817f5e7e78327e54
Jakub Klinkovský <j.l.k@gmx.com>
2018-08-16 18:36:06 +0200

    implement recursive template expansion

--- tests/parser_helpers/test_wikicode.py
+++ tests/parser_helpers/test_wikicode.py
@@ -3,6 +3,7 @@
 import mwparserfromhell
 
 from ws.parser_helpers.wikicode import *
+from ws.parser_helpers.title import canonicalize
 
 class test_get_adjacent_node:
     def test_basic(self):
@@ -386,3 +387,124 @@ class test_ensure_unflagged:
         link = wikicode.nodes[0]
         flag = ensure_unflagged_by_template(wikicode, link, "bar")
         assert str(wikicode) == "[[foo]]"
+
+class test_expand_templates:
+    @staticmethod
+    def _do_test(d, title, expected):
+        def content_getter(title):
+            nonlocal d
+            try:
+                return d[title]
+            except KeyError:
+                raise ValueError
+
+        content = d[title]
+        wikicode = mwparserfromhell.parse(content)
+        expand_templates(title, wikicode, content_getter)
+        assert wikicode == expected
+
+    def test_basic(self):
+        d = {
+            "Template:Note": "{{{1}}}",
+            "Title": "{{Note|{{Note|foo}}}}"
+        }
+        title = "Title"
+        expected = "foo"
+        self._do_test(d, title, expected)
+
+    def test_default_values(self):
+        d = {
+            "Template:Note": "{{{1|default}}}",
+            "Title": "{{Note|{{Note}}}} {{Note|}} {{Note|non-default}}"
+        }
+        title = "Title"
+        expected = "default  non-default"
+        self._do_test(d, title, expected)
+
+    def test_invalid_template(self):
+        d = {
+            "Title": "{{invalid}}"
+        }
+        title = "Title"
+        expected = "[[Template:Invalid]]"
+        self._do_test(d, title, expected)
+
+    def test_invalid_page(self):
+        d = {
+            "Title": "{{:invalid}}"
+        }
+        title = "Title"
+        expected = "[[Invalid]]"
+        self._do_test(d, title, expected)
+
+    def test_infinite_loop_protection(self):
+        d = {
+            "Template:A": "a: {{b}}",
+            "Template:B": "b: {{c}}",
+            "Template:C": "c: {{:title}}",
+            "Title": "{{a}}",
+        }
+        title = "Title"
+        expected = "a: b: c: {{a}}"
+        self._do_test(d, title, expected)
+
+    def test_relative_transclusion(self):
+        d = {
+            "Template:A": "a: {{/B}}",
+            "Template:A/B": "b: {{c}}",
+            "Title": "{{a}}",
+        }
+        title = "Title"
+        # this is very weird, but that's what MediaWiki does...
+        expected = "a: [[Title/B]]"
+        self._do_test(d, title, expected)
+
+    def test_named_parameers(self):
+        d = {
+            "Template:A": "a: {{{a|b: {{{b|c: {{{c|}}} }}} }}}",
+            "Title 1": "x{{A}}y",
+            "Title 2": "x{{A|a=foo}}y",
+            "Title 3": "x{{A|b=foo}}y",
+            "Title 4": "x{{A|c=foo}}y",
+        }
+
+        title = "Title 1"
+        expected = "xa: b: c:   y"
+        self._do_test(d, title, expected)
+
+        title = "Title 2"
+        expected = "xa: fooy"
+        self._do_test(d, title, expected)
+
+        title = "Title 3"
+        expected = "xa: b: foo y"
+        self._do_test(d, title, expected)
+
+        title = "Title 4"
+        expected = "xa: b: c: foo  y"
+        self._do_test(d, title, expected)
+
+    def test_nested_argument_name(self):
+        d = {
+            "Template:A": "{{{ {{{1}}} |foo }}}",
+            "Title 1": "x{{A}}y",
+            "Title 2": "x{{A|1|bar}}y",
+            "Title 3": "x{{A|2|bar}}y",
+            "Title 4": "x{{A|3|bar}}y",
+        }
+
+        title = "Title 1"
+        expected = "xfoo y"
+        self._do_test(d, title, expected)
+
+        title = "Title 2"
+        expected = "x1y"
+        self._do_test(d, title, expected)
+
+        title = "Title 3"
+        expected = "xbary"
+        self._do_test(d, title, expected)
+
+        title = "Title 4"
+        expected = "xfoo y"
+        self._do_test(d, title, expected)
--- ws/parser_helpers/wikicode.py
+++ ws/parser_helpers/wikicode.py
@@ -5,8 +5,13 @@ import re
 import mwparserfromhell
 
 from .encodings import dotencode
+from .title import canonicalize
 
-__all__ = ["strip_markup", "get_adjacent_node", "get_parent_wikicode", "remove_and_squash", "get_section_headings", "get_anchors", "ensure_flagged_by_template", "ensure_unflagged_by_template"]
+__all__ = [
+    "strip_markup", "get_adjacent_node", "get_parent_wikicode", "remove_and_squash",
+    "get_section_headings", "get_anchors", "ensure_flagged_by_template",
+    "ensure_unflagged_by_template", "expand_templates",
+]
 
 def strip_markup(text, normalize=True, collapse=True):
     """
@@ -215,3 +220,94 @@ def ensure_unflagged_by_template(wikicode, node, template_name):
 
     if isinstance(adjacent, mwparserfromhell.nodes.Template) and adjacent.name.matches(template_name):
         remove_and_squash(wikicode, adjacent)
+
+def expand_templates(title, wikicode, content_getter_func, *, template_prefix="Template"):
+    """
+    Recursively expands all templates on a MediaWiki page.
+
+    :param str title:
+        The title of the page where the templates will be expanded. Used for
+        infinite loop prevention and context (e.g. relative transclusions like
+        ``{{/foo/bar}}``).
+    :param wikicode:
+        The content of the page where templates should be expanded, as a
+        :py:class:`mwparserfromhell.wikicode.Wikicode` object.
+    :param content_getter_func:
+        A callback function which should return the content of a transcluded
+        page. It is called as ``content_getter_func(name)``, where the string
+        ``name`` is the canonical name of the transcluded page. For example,
+        in ``{{ foo bar | baz }}``, the ``name`` is ``"Template:foo bar"``. The
+        function should raise :py:exc:`ValueError` if the requested page does
+        not exist.
+    :returns: ``None``, the wikicode is modified in place.
+    """
+    if not isinstance(wikicode, mwparserfromhell.wikicode.Wikicode):
+        raise TypeError("wikicode is of type {} instead of mwparserfromhell.wikicode.Wikicode".format(type(wikicode)))
+
+    # TODO: handle <noinclude>, <includeonly>, <onlyinclude> tags
+    # reference: https://www.mediawiki.org/wiki/Transclusion#Partial_transclusion
+    def prepare_template_content(wikicode, template):
+        """
+        :param wikicode: the wikicode of the template
+        :param template: the template object holding parameters for substitution
+        """
+        # substitute template arguments
+        for arg in wikicode.ifilter_arguments(recursive=wikicode.RECURSE_OTHERS):
+            # handle nested substitution like {{{ {{{1}}} |foo }}}
+            prepare_template_content(arg.name, template)
+            try:
+                param_value = template.get(arg.name).value
+            except ValueError:
+                param_value = arg.default
+            # If a template contains e.g. {{{1}}} and no corresponding parameter is given,
+            # MediaWiki renders "{{{1}}}" verbatim.
+            if param_value is not None:
+                # handle nested substitution like {{{a| {{{b| {{{c|}}} }}} }}}
+                prepare_template_content(param_value, template)
+                wikicode.replace(arg, param_value)
+
+    # TODO: this should be done in the Title class (canonicalization depends on whether the namespace is first-letter case-sensitive or not)
+    def handle_relative_title(src_title, title):
+        nonlocal template_prefix
+        if title.startswith("/"):
+            return src_title + title
+        elif title.startswith(":"):
+            return canonicalize(title[1:])
+        else:
+            return template_prefix + ":" + canonicalize(title)
+
+    def expand(title, wikicode, content_getter_func, visited_pages):
+        """
+        Adds infinite loop protection to the functionality declared by :py:func:`expand_templates`.
+        """
+        for template in wikicode.ifilter_templates(recursive=wikicode.RECURSE_OTHERS):
+            # handle cases like {{ {{foo}} | bar }} --> {{foo}} has to be substituted first
+            expand(title, template.name, content_getter_func, visited_pages)
+            name = canonicalize(str(template.name))
+            target_title = canonicalize(handle_relative_title(title, name))
+            try:
+                content = content_getter_func(target_title)
+            except ValueError:
+                # If the target page does not exist, MediaWiki just skips the expansion,
+                # but it renders a wikilink to the non-existing page.
+                # TODO: the wikilink appears both in pagelinks and templatelinks tables !!!
+                wikicode.replace(template, "[[{}]]".format(handle_relative_title(title, str(template.name))))
+                continue
+
+            # Note:
+            # MW has a special case when the first character produced by the template is one of ":;*#", MediaWiki inserts a linebreak
+            # reference: https://en.wikipedia.org/wiki/Help:Template#Problems_and_workarounds
+            # TODO: check what happens in our case
+            content = mwparserfromhell.parse(content)
+            prepare_template_content(content, template)
+
+            # Expand only if the infinite loop checker does not kick in, otherwise we just leave it unexpanded.
+            if target_title not in visited_pages:
+                visited_pages.add(target_title)
+                expand(title, content, content_getter_func, visited_pages)
+                visited_pages.remove(target_title)
+
+            wikicode.replace(template, content)
+
+    title = canonicalize(title)
+    expand(title, wikicode, content_getter_func, {title})
