73f6c377e4eb9dd2b0c91f320b2f9f57639fbd56
Jakub Klinkovský <j.l.k@gmx.com>
2020-08-03 16:57:35 +0200

    ManTemplateChecker: use check_url from ExtlinkStatusChecker, fix heuristics for missing section number

--- ws/checkers/ManTemplateChecker.py
+++ ws/checkers/ManTemplateChecker.py
@@ -5,7 +5,8 @@ import datetime
 
 import requests
 
-from .CheckerBase import localize_flag, CheckerBase
+from .CheckerBase import localize_flag
+from .ExtlinkStatusChecker import ExtlinkStatusChecker
 import ws.ArchWiki.lang as lang
 from ws.parser_helpers.encodings import queryencode
 from ws.parser_helpers.wikicode import ensure_flagged_by_template, ensure_unflagged_by_template
@@ -15,20 +16,11 @@ __all__ = ["ManTemplateChecker"]
 logger = logging.getLogger(__name__)
 
 
-class ManTemplateChecker(CheckerBase):
-    man_url_prefix = "http://jlk.fjfi.cvut.cz/arch/manpages/man/"
+class ManTemplateChecker(ExtlinkStatusChecker):
+    man_url_prefix = "https://jlk.fjfi.cvut.cz/arch/manpages/man/"
 
-    def __init__(self, api, db, *, timeout=30, max_retries=3, **kwargs):
+    def __init__(self, api, db, **kwargs):
         super().__init__(api, db, **kwargs)
-
-        self.timeout = timeout
-        self.session = requests.Session()
-        adapter = requests.adapters.HTTPAdapter(max_retries=max_retries)
-        self.session.mount("https://", adapter)
-        self.session.mount("http://", adapter)
-
-        self.cache_valid_urls = set()
-        self.cache_invalid_urls = set()
 
     def update_man_template(self, wikicode, template, src_title):
         if template.name.lower() != "man":
@@ -51,6 +43,13 @@ class ManTemplateChecker(CheckerBase):
         if template.has("pkg"):
             url += template.get("pkg").value.strip() + "/"
         url += queryencode(template.get(2).value.strip())
+        # template parameter 1= should be empty
+        if not template.has(1, ignore_empty=True):
+            response = self.session.head(url, timeout=self.timeout, allow_redirects=True)
+            # heuristics to get the missing section (redirect from some_page to some_page.1)
+            # WARNING: if the manual exists in multiple sections, the first one might not be the best
+            if response.status_code == 200 and len(response.history) == 1 and response.url.startswith(url + "."):
+                template.add(1, response.url[len(url) + 1:])
         if template.get(1).value.strip():
             url += "." + template.get(1).value.strip()
         if template.has(3):
@@ -61,35 +60,8 @@ class ManTemplateChecker(CheckerBase):
         else:
             explicit_url = None
 
-        def check_url(url):
-            if url.startswith("ftp://"):
-                logger.error("The FTP protocol is not supported by the requests module. URL: {}".format(url))
-                return True
-            if url in self.cache_valid_urls:
-                return True
-            elif url in self.cache_invalid_urls:
-                return False
-            response = self.session.get(url, timeout=self.timeout)
-            if response.status_code == 200:
-                # heuristics to get the missing section (redirect from some_page to some_page.1)
-                # WARNING: if the manual exists in multiple sections, the first one might not be the best
-                if len(response.history) == 1 and response.url.startswith(url + "."):
-                    # template parameter 1= should be empty
-                    assert not template.has(1, ignore_empty=True)
-                    template.add(1, response.url[len(url) + 1:])
-                    self.cache_valid_urls.add(response.url)
-                    return True
-                else:
-                    self.cache_valid_urls.add(url)
-                    return True
-            elif response.status_code >= 400:
-                self.cache_invalid_urls.add(url)
-                return False
-            else:
-                raise NotImplementedError("Unexpected status code {} for man page URL: {}".format(response.status_code, url))
-
         # check if the template parameters form a valid URL
-        if check_url(url):
+        if self.check_url(url):
             ensure_unflagged_by_template(wikicode, template, "Dead link", match_only_prefix=True)
             # remove explicit url= parameter - not necessary
             if explicit_url is not None:
@@ -101,7 +73,7 @@ class ManTemplateChecker(CheckerBase):
             # flag with the correct translated template
             ensure_flagged_by_template(wikicode, template, flag, *deadlink_params, overwrite_parameters=False)
         elif explicit_url != "":
-            if check_url(explicit_url):
+            if self.check_url(explicit_url):
                 ensure_unflagged_by_template(wikicode, template, "Dead link", match_only_prefix=True)
             else:
                 # first replace the existing template (if any) with a translated version
