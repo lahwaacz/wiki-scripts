4db1eadbaf6d47e5ce6fb2c917fb63b84a447cee
windowsboy111 <wboy111@outlook.com>
2021-05-30 22:58:56 +0800

    tidy up

--- localize-templates.py
+++ localize-templates.py
@@ -3,10 +3,8 @@
 This script aims to replace unlocalised templates in localised pages with the localised templates.
 '''
 import os
-import contextlib
 from ws.client import API
 import ws.ArchWiki.lang as lang
-import mwparserfromhell
 
 
 localised_lang_name = "简体中文" # simplified Chinese (zh-Hans)
@@ -14,13 +12,13 @@ templates = ["Translateme"]
 api_url = "https://wiki.archlinux.org/api.php"
 index_url = "https://wiki.arclinux.org/index.php"
 cookie_path = os.path.expanduser("~/.cache/ArchWiki.cookie")
-session = API.make_session(
+session = API.make_session(ssl_verify=True,
                            cookie_file=cookie_path)
 
 
 def edit(api: API, timestamp: str, text: str, page, template: str):
     new = text.replace(r"{{" + template, r"{{" + template + f" ({localised_lang_name})")
-    # api.edit(pageid=page['pageid'], title=page['title'], basetimestamp=timestamp, text=new, summary="localize templates", bot="")
+    api.edit(pageid=page['pageid'], title=page['title'], basetimestamp=timestamp, text=new, summary="localize templates", bot="")
     print(f"Edited {page['title']}")
 
 
@@ -57,8 +55,12 @@ def main(template: str):
     for page in pages:
         text = ""
         try:
-            text = page['revisions'][0]['slots']['main']['content'] if firstRet else ret['pages'][str(page['pageid'])]['revisions'][0]['slots']['main']['*']
-            timestamp = page['revisions'][0]['timestamp'] if firstRet else ret['pages'][str(page['pageid'])]['revisions'][0]['timestamp']
+            if firstRet:
+                text = page['revisions'][0]['slots']['main']['content']
+                timestamp = page['revisions'][0]['timestamp']
+            else:
+                text = ret['pages'][str(page['pageid'])]['revisions'][0]['slots']['main']['*']
+                timestamp = ret['pages'][str(page['pageid'])]['revisions'][0]['timestamp']
         except KeyError:
             firstRet = False
             print(end="Calling API: get content for remaining pages...", flush=True)
@@ -72,7 +74,7 @@ def main(template: str):
 
     for i, text in enumerate(texts):
         edit(api, timestamps[i], text, pages[i], template)
-    
+
 
 if __name__ == '__main__':
     for template in templates:
