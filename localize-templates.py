571898fcb59ce42c70294cb8e6fe7e15da686fbd
windowsboy111 <wboy111@outlook.com>
2021-06-13 11:30:50 +0800

    supports bulk template editing; bug fixes use edit_interactive

--- localize-templates.py
+++ localize-templates.py
@@ -1,97 +1,107 @@
 #! /usr/bin/env python3
-'''
+"""
 This script aims to replace unlocalised templates in localised pages with the localised templates.
-'''
+
+"""
 import os
 import copy
+from ws.interactive import edit_interactive
 from ws.client import API
-import ws.ArchWiki.lang as lang
+from ws.ArchWiki import lang
 import mwparserfromhell as mw
 
-
-localised_lang_name: str = "简体中文" # not required, can omit val; simplified Chinese (zh-Hans)
-templates: list[str] = ["Translateme"]
 api_url = "https://wiki.archlinux.org/api.php"
 index_url = "https://wiki.arclinux.org/index.php"
 cookie_path = os.path.expanduser("~/.cache/ArchWiki.cookie")
 session = API.make_session(ssl_verify=True,
                            cookie_file=cookie_path)
 
+dl = lambda page: lang.detect_language(page["title"])[1]
+dfkw = {"format": "json", "utf8": 1, "formatversion": 2}
 
-def edit(api: API, timestamp: str, text: str, page, template: str, language: str):
+
+def ukw(kw: dict):
+    """add common attribs on kw"""
+    k = dfkw.copy()
+    k.update(kw)
+    return k
+
+
+def get_templates(api: API):
+    """Get templates with i18n"""
+    ret = api.list(**ukw({
+        "prop": "langlinks",
+        "list": "allpages",
+        "lllang": "zh-hant",
+        "lllimit": "max",
+        "apnamespace": "10",
+        "apfilterlanglinks": "withlanglinks",
+    }))
+    return (page["title"] for page in ret if dl(page) == "English")
+
+
+def edit(api: API, page, templates):
+    text = page["revisions"][0]["slots"]["main"]["content"]
+    timestamp = page["revisions"][0]["timestamp"]
     code = mw.parse(text)
     for curTemplate in code.filter_templates():
-        if curTemplate.name.matches(template):
-            curTemplate.name = f"{template} ({language})"
-    text = str(code)
-    api.edit(pageid=page['pageid'], title=page['title'], basetimestamp=timestamp, text=new, summary="localize templates", bot="")
-    print(f"Edited {page['title']}")
-
-
-def process(template: str, language: str):
-    pages = []
-    texts: list[str] = []
-    timestamps: list[str] = []
-    api = API(api_url, index_url, session)
-    kwargs = {
-        "action": "query",
-        "format": "json",
-        "maxlag": "100",
-        "prop": "revisions",
-        "continue": "",
-        "generator": "embeddedin",
-        "redirects": 1,
-        "utf8": 1,
-        "formatversion": "2",
-        "rvprop": "content|timestamp",
-        "rvslots": "main",
-        "geititle": f"Template:{template}",
-        "geifilterredir": "nonredirects",
-        "geilimit": "max"
-    }
-    # get page content
-    print(end="Calling API: query generator...", flush=True)
-    ret = api.call_api(**kwargs)
-    for page in ret['pages']:
-        if lang.detect_language(page['title'])[1] == language:
-            pages.append(page)
-    print("Done")
-
-    firstRet = True
-    for page in pages:
-        text = ""
-        try:
-            if firstRet:
-                text = page['revisions'][0]['slots']['main']['content']
-                timestamp = page['revisions'][0]['timestamp']
-            else:
-                text = ret['pages'][str(page['pageid'])]['revisions'][0]['slots']['main']['*']
-                timestamp = ret['pages'][str(page['pageid'])]['revisions'][0]['timestamp']
-        except KeyError:
-            firstRet = False
-            print(end="Calling API: get content for remaining pages...", flush=True)
-            ret = api.call_api(action="query", format="json", prop="revisions", rvprop="content|timestamp", rvslots="main", titles="|".join(page['title'] for page in pages))
-            print("OK")
-            text = ret['pages'][str(page['pageid'])]['revisions'][0]['slots']['main']['*']
-            timestamp = ret['pages'][str(page['pageid'])]['revisions'][0]['timestamp']
-        texts.append(text)
-        timestamps.append(timestamp)
-        print(f"Checked {page['title']}")
-
-    for i, text in enumerate(texts):
-        edit(api, timestamps[i], text, pages[i], template, language)
-    return
+        for template in templates:
+            if curTemplate.name.matches(template[9:]):
+                curTemplate.name = f"{template[9:]} ({dl(page)})"
+    if __name__ == "__main__":
+        edit_interactive(api, page["title"], page["pageid"], text, str(code), timestamp, "localize templates")
+    else:
+        api.edit(page['title'], page['pageid'], str(code), timestamp, "localize templates", bot="")
+        print(f"Edited {page['title']}")
 
 
 def main():
-    if localised_lang_name:
-        for template in templates:
-            process(template, localised_lang_name)
-        return
-    for language in lang.get_language_names(): # just in case language is not specified
-        for template in templates:
-            process(template, language)
-    return
+    pages: list = []
+    api = API(api_url, index_url, session)
+    gentmps = get_templates(api)
+    tmps: list = []
+    print(end=" Getting pages...\r", flush=True)
+    for i, template in enumerate(gentmps):
+        tmps.append(template)
+        # get page content
+        ret = api.query_continue(
+                **ukw(
+                    {
+                        "generator": "embeddedin",
+                        "prop": "revisions",
+                        "redirects": 1,
+                        "rvprop": "content|timestamp",
+                        "rvslots": "main",
+                        "geilimit": "max",
+                        "geifilterredir": "nonredirects",
+                        "geititle": template,
+                    }
+                )
+            )
+        for bn, block in enumerate(ret):
+            # ^ generators just require a for loop for the blks
+            for page in block["pages"]:
+                # no English; no duplicates; no namespaces
+                if (
+                    dl(page) != "English"
+                    and page not in pages
+                    and ":" not in page["title"]
+                ):
+                    try:
+                        page["revisions"][0]  # test if revisions entry exists, else KeyError
+                        pages.append(page)
+                        print(
+                            end=f"\033[2K Getting pages... {len(pages)} done | #{i+1}: {template[9:]} | blk #{bn+1} | {page['title']}\r",
+                            flush=True,
+                        )
+                    except KeyError:
+                        break  # exit the page loop, next block
+    print(f"\033[2K{len(pages)} pages retrieved")
 
-if __name__ == '__main__':
+    for page in pages:
+        print(f"~~ Editing {page['title']} ~~")
+        edit(api, page, tmps)
+
+
+if __name__ == "__main__":
     main()
