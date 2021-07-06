80440065690e76bdfc8a309f250145d8dd2dd486
Jakub Klinkovský <j.l.k@gmx.com>
2021-07-06 09:56:12 +0200

    localize-templates.py: optimized getting page contents
    Fetching the revision content is split into a single outer loop,
    otherwise the content might be fetched multiple times (e.g. when
    multiple templates are used on one page).

--- localize-templates.py
+++ localize-templates.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python3
 
-import os
 from ws.interactive import edit_interactive
 from ws.client import API
+from ws.utils import dmerge
 from ws.ArchWiki import lang
-import mwparserfromhell as mw
+import mwparserfromhell
 
 dl = lambda page: lang.detect_language(page["title"])[1]
 dfkw = {"format": "json", "utf8": 1, "formatversion": 2}
@@ -30,9 +30,10 @@ def get_templates(api: API):
 
 
 def edit(api: API, page, templates):
-    text = page["revisions"][0]["slots"]["main"]["content"]
+    print(f"Parsing '{page['title']}'...")
+    text = page["revisions"][0]["slots"]["main"]["*"]
     timestamp = page["revisions"][0]["timestamp"]
-    code = mw.parse(text)
+    code = mwparserfromhell.parse(text)
     for curTemplate in code.filter_templates():
         for template in templates:
             if curTemplate.name.matches(template[9:]):
@@ -45,50 +46,32 @@ def edit(api: API, page, templates):
 
 
 def main(api: API):
-    pages: list = []
-    gentmps = get_templates(api)
-    tmps: list = []
-    print(end=" Getting pages...\r", flush=True)
-    for i, template in enumerate(gentmps):
-        tmps.append(template)
-        # get page content
-        ret = api.query_continue(
-                **ukw(
-                    {
-                        "generator": "embeddedin",
-                        "prop": "revisions",
-                        "redirects": 1,
-                        "rvprop": "content|timestamp",
-                        "rvslots": "main",
-                        "geilimit": "max",
-                        "geifilterredir": "nonredirects",
-                        "geititle": template,
-                    }
-                )
-            )
-        for bn, block in enumerate(ret):
-            # ^ generators just require a for loop for the blks
-            for page in block["pages"]:
-                # no English; no duplicates; no namespaces
-                if (
-                    dl(page) != "English"
-                    and page not in pages
-                    and ":" not in page["title"]
-                ):
-                    try:
-                        page["revisions"][0]  # test if revisions entry exists, else KeyError
-                        pages.append(page)
-                        print(
-                            end=f"\033[2K Getting pages... {len(pages)} done | #{i+1}: {template[9:]} | blk #{bn+1} | {page['title']}\r",
-                            flush=True,
-                        )
-                    except KeyError:
-                        break  # exit the page loop, next block
-    print(f"\033[2K{len(pages)} pages retrieved")
+    print("Getting page IDs...")
+    pageids = set()
+    templates = []
+    for template in get_templates(api):
+        templates.append(template)
+        # get IDs of the pages using this template
+        for page in api.generator(generator="embeddedin",
+                                  geifilterredir="nonredirects",
+                                  geilimit="max",
+                                  geititle=template):
+            if dl(page) != "English":
+                pageids.add(page["pageid"])
+    print(f"Fetched {len(pageids)} pages.")
 
-    for page in pages:
-        print(f"~~ Editing {page['title']} ~~")
-        edit(api, page, tmps)
+    print("Getting page contents...")
+    result = {}
+    for chunk in api.call_api_autoiter_ids(action="query",
+                                           pageids=pageids,
+                                           prop="revisions",
+                                           rvprop="content|timestamp",
+                                           rvslots="main"):
+        dmerge(chunk, result)
+
+    pages = result["pages"]
+    for page in pages.values():
+        edit(api, page, templates)
 
 
 if __name__ == "__main__":
