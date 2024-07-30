bb6b196f6685d9ed9e2027d68ad95988c5f1370f
Jakub Klinkovský <1289205+lahwaacz@users.noreply.github.com>
2024-07-30 23:00:51 +0200

    ExtlinkStatusChecker: switch to Firefox user agent
    This allows to check more domains reliably, e.g. all Intel sites.

--- ws/checkers/ExtlinkStatusChecker.py
+++ ws/checkers/ExtlinkStatusChecker.py
@@ -96,7 +96,7 @@ class ExtlinkStatusChecker:
         timeout = httpx.Timeout(timeout, pool=None)  # disable timeout for waiting for a connection from the pool
         headers = {
             # fake user agent to bypass servers responding differently or not at all to non-browser user agents
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:128.0) Gecko/20100101 Firefox/128.0",
         }
 
         # create an SSL context to disallow TLS1.0 and TLS1.1, allow only TLS1.2
