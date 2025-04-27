8935491a1a1a8b2d38b9fa9b15566d4c6b7ab6fd
Jakub Klinkovský <1289205+lahwaacz@users.noreply.github.com>
2025-04-27 22:13:30 +0200

    tests: drop removed option for postgresql_proc fixture

--- tests/fixtures/postgresql.py
+++ tests/fixtures/postgresql.py
@@ -8,7 +8,7 @@ pg_executable = "/usr/bin/pg_ctl"
 db_name = "wiki_scripts"
 
 # postgresql process fixture
-postgresql_proc = factories.postgresql_proc(logs_prefix="pytest-", executable=pg_executable)
+postgresql_proc = factories.postgresql_proc(executable=pg_executable)
 # fixture holding an instance of a psycopg connection
 postgresql = factories.postgresql("postgresql_proc", dbname=db_name)
 
