066109ca4e10f6d15cd6f0f34c5d04ec8733e29d
Jakub Klinkovský <1289205+lahwaacz@users.noreply.github.com>
2025-05-03 22:42:41 +0200

    tests: remove unused fixture api_archwiki

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,9 @@
 #! /usr/bin/env python3
 
-import pytest
 from fixtures.containers import *
 from fixtures.database import *
 from fixtures.mediawiki import *
 from fixtures.title_context import *
-
-from ws.client.api import API
 
 
 # disable rate-limiting for tests
@@ -17,14 +14,3 @@ def pytest_configure(config):
 def pytest_unconfigure(config):
     import ws
     del ws._tests_are_running
-
-@pytest.fixture(scope="session")
-def api_archwiki():
-    """
-    Return an API instance with anonymous connection to wiki.archlinux.org
-    """
-    # NOTE: anonymous, will be very slow for big data!
-    api_url = "https://wiki.archlinux.org/api.php"
-    index_url = "https://wiki.archlinux.org/index.php"
-    session = API.make_session()
-    return API(api_url, index_url, session)
