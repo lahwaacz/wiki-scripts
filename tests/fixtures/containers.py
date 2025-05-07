f7b63a0e47f73a03511a636b08232d0fffdba2df
Jakub Klinkovský <1289205+lahwaacz@users.noreply.github.com>
2025-05-07 22:50:06 +0200

    tests: add --keep-containers-running option

--- tests/fixtures/containers.py
+++ tests/fixtures/containers.py
@@ -4,12 +4,32 @@ import dotenv
 import pytest
 
 __all__ = [
+    "pytest_addoption",
+    "keep_containers_running",
     "docker_compose_file",
     "docker_compose_project_name",
     "docker_setup",
     "docker_cleanup",
     "containers_dotenv_values",
 ]
+
+
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption(
+        "--keep-containers-running",
+        action="store_true",
+        help="Keep containers running after the test (i.e. don't run `docker compose down`).",
+    )
+
+
+@pytest.fixture(scope="session")
+def keep_containers_running(request: pytest.FixtureRequest) -> bool:
+    """
+    Bool fixture determining whether to keep containers running after the test
+    (i.e. don't run `docker compose down`).
+    """
+    value = request.config.getoption("--keep-containers-running")
+    return bool(value)
 
 
 @pytest.fixture(scope="session")
@@ -24,20 +44,25 @@ def docker_compose_project_name() -> str:
     return "wiki-scripts-tests"
 
 
-# Stop the stack before starting a new one
 @pytest.fixture(scope="session")
-def docker_setup():
+def docker_setup(keep_containers_running: bool) -> list[str]:
+    """Spin up containers for the pytest session using docker compose."""
+    if keep_containers_running:
+        return ["up --build --detach"]
     return ["down --remove-orphans --volumes", "up --build --detach"]
 
 
-# Cleanup anonymous volumes created by the compose
 @pytest.fixture(scope="session")
-def docker_cleanup():
+def docker_cleanup(keep_containers_running: bool) -> list[str]:
+    """Tear down containers for the pytest session using docker compose."""
+    if keep_containers_running:
+        return []
     return ["down --remove-orphans --volumes"]
 
 
 @pytest.fixture(scope="session")
 def containers_dotenv_values(docker_compose_file: str) -> dict[str, str | None]:
+    """Return a dict of environment variables configured in the `.env` file of the compose project."""
     dotenv_path = Path(docker_compose_file).parent / ".env"
     assert dotenv_path.is_file()
     return dotenv.dotenv_values(dotenv_path)
