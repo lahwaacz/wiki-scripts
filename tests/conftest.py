72885cdf0e50bca4d938f8ee6f8b5890a452c162
Jakub Klinkovský <1289205+lahwaacz@users.noreply.github.com>
2025-05-07 23:15:08 +0200

    tests: replace pytest_configure/pytest_unconfigure with a global autouse fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,6 @@ from tests.fixtures.mediawiki import *
 from tests.fixtures.title_context import *
 
 
-# disable rate-limiting for tests
-def pytest_configure(config: pytest.Config) -> None:
-    ws._tests_are_running = True
-
-def pytest_unconfigure(config: pytest.Config) -> None:
-    del ws._tests_are_running
+@pytest.fixture(autouse=True)
+def disable_rate_limiting(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setattr(ws, "_tests_are_running", True, raising=False)
