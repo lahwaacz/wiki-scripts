3fd2cd49d384d319143dce31c3375f2fb7e91ccd
Jakub Klinkovský <1289205+lahwaacz@users.noreply.github.com>
2025-05-30 17:23:49 +0200

    scripts: add type hints and reformat

--- update-page-language.py
+++ update-page-language.py
@@ -5,13 +5,15 @@ from ws.client import API
 from ws.interactive import require_login
 
 
-def update_page_language(api):
+def update_page_language(api: API) -> None:
     # ensure that we are authenticated
     require_login(api)
 
     namespaces = [0, 4, 10, 12, 14]
     for ns in namespaces:
-        for page in api.generator(generator="allpages", gapnamespace=ns, gaplimit="max", prop="info"):
+        for page in api.generator(
+            generator="allpages", gapnamespace=ns, gaplimit="max", prop="info"
+        ):
             title = page["title"]
             pagelanguage = page["pagelanguage"]
 
@@ -19,10 +21,16 @@ def update_page_language(api):
             langtag = lang.tag_for_langname(langname)
 
             if pagelanguage != langtag:
-                api.set_page_language(title, langtag, "update language based on the page title")
+                api.set_page_language(
+                    title, langtag, "update language based on the page title"
+                )
+
 
 if __name__ == "__main__":
     import ws.config
 
-    api = ws.config.object_from_argparser(API, description="Updates the page language property in the wiki's database based on the ArchWiki page naming: https://wiki.archlinux.org/title/Help:I18n#Page_titles")
+    api = ws.config.object_from_argparser(
+        API,
+        description="Updates the page language property in the wiki's database based on the ArchWiki page naming: https://wiki.archlinux.org/title/Help:I18n#Page_titles",
+    )
     update_page_language(api)
